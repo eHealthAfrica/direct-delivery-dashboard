09b5964dc3693bad79f0c8abfa9bb46a3e81d3a2
Haykel Ben Jemia <hbj@allmas.tn>
2014-05-06 11:13:18 +0100

    Start rewrite of application - base structure.

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular
+  .module('smart2App', [
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
