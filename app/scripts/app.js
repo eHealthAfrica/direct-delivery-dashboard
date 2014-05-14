05d6ee45de994527b70fa01934afd4cab1d9e2a3
Haykel Ben Jemia <hbj@allmas.tn>
2014-05-14 12:09:40 +0100

    Implement stock chart. Fixes #452.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular
   .module('lmisApp', [
     'ngSanitize',
     'ngRoute',
-    'pouchdb'
+    'pouchdb',
+    'nvd3ChartDirectives'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,8 +7,16 @@ angular.module('lmisApp')
   })
   .controller('UnopenedCtrl', function ($scope, stockcountUnopened) {
     $scope.rows = [];
+    $scope.chartData = [];
+    $scope.chartFacility = '';
     $scope.loading = true;
     $scope.error = false;
+
+    $scope.formatDateAxisFunction = function () {
+      return function (d) {
+        return d3.time.format('%Y-%m-%d')(new Date(d));
+      }
+    };
 
     $scope.count = function (row, productId) {
       var product = row.products[productId];
@@ -20,9 +28,52 @@ angular.module('lmisApp')
       return (product && product.count < product.min);
     };
 
+    $scope.setChartData = function(facility) {
+      if (!facility)
+        return;
+
+      var products = {};
+
+      $scope.rows.forEach(function (row) {
+        if (row.facility == facility) {
+          Object.keys(row.products).forEach(function (key) {
+            if (!products[key]) {
+              products[key] = {
+                'key': ($scope.products[key] || key).substr(0, 10),
+                'values': []
+              }
+            }
+
+            products[key].values.push([row.date.getTime(), row.products[key].count]);
+          });
+        }
+      });
+
+      $scope.chartFacility = facility;
+      $scope.chartData = Object.keys(products).map(function (key) {
+        // sort values by date
+        products[key].values.sort(function(a, b) {
+          if (a[0] < b[0])
+            return -1;
+          else if (a[0] > b[0])
+            return 1;
+          else
+            return 0;
+        });
+
+        return products[key];
+      });
+    }
+
     stockcountUnopened.byFacilityAndDate()
       .then(function (rows) {
         $scope.rows = prepare(rows);
+        var firstChart = '';
+        $scope.rows.forEach(function(row) {
+          if (!firstChart || row.facility < firstChart)
+            firstChart = row.facility;
+        });
+        $scope.setChartData(firstChart);
       })
       .catch(function () {
         $scope.error = true;
