c26e5280bb6ac31aba48ff02bafc3b8c11f67118
Haykel Ben Jemia <hbj@allmas.tn>
2014-06-14 12:51:43 +0100

    Add place and timeframe filtered table of totals for CCU breakdowns.

--- app/scripts/controllers/admin-views.js
+++ app/scripts/controllers/admin-views.js
@@ -1,18 +1,129 @@
 'use strict';
 
 angular.module('lmisApp')
-  .controller('CCUBreakdownCtrl', function ($scope, ccuBreakdown) {
+  .controller('CCUBreakdownCtrl', function ($scope, State, Zone, LGA, Ward, Facility, ccuBreakdown) {
     $scope.rows = [];
+    $scope.totals = [];
     $scope.loading = true;
     $scope.error = false;
+    $scope.loadingPlaces = false;
+
+    $scope.place = {
+      type: 0,
+      columnTitle: 'Zone',
+      search: ''
+    };
+
+    $scope.from = {
+      opened: false,
+      date: moment().startOf('day').subtract('days', 7).toDate(),
+      open: function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        this.opened = true;
+      }
+    };
+
+    $scope.to = {
+      opened: false,
+      date: moment().endOf('day').subtract('days', 1).toDate(),
+      open: function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        this.opened = true;
+      }
+    };
+
+    $scope.getPlaces = function(value) {
+      $scope.loadingPlaces = true;
+      var service = State;
+      switch (parseInt($scope.place.type)) {
+        case 1:
+          service = Zone;
+          break;
+        case 2:
+          service = LGA;
+          break;
+        case 3:
+          service = Ward;
+          break;
+        case 4:
+          service = Facility;
+          break;
+      }
+
+      return service.names(value)
+        .finally(function () {
+          $scope.loadingPlaces = false;
+        }.bind(this));
+    };
+
+    $scope.updateTotals = function () {
+      var totals = {};
+      var filterBy = 'state';
+      var groupBy = 'zone';
+      var columnTitle = 'Zone';
+      switch (parseInt($scope.place.type)) {
+        case 1:
+          filterBy = 'zone';
+          groupBy = 'lga';
+          columnTitle = 'LGA';
+          break;
+        case 2:
+          filterBy = 'lga';
+          groupBy = 'ward';
+          columnTitle = 'Ward';
+          break;
+        case 3:
+          filterBy = 'ward';
+          groupBy = 'facility';
+          columnTitle = 'Facility';
+          break;
+        case 4:
+          filterBy = 'facility';
+          groupBy = 'facility';
+          columnTitle = 'Facility';
+          break;
+      }
+
+      if ($scope.place.search.length) {
+        var search = $scope.place.search.toLowerCase();
+        $scope.rows.forEach(function (row) {
+          if (row[filterBy].toLowerCase() == search) {
+            var key = row[groupBy] + '#' + row.name;
+            totals[key] = totals[key] || {
+              place: row[groupBy],
+              name: row.name,
+              total: 0
+            };
+
+            var date = moment(row.created);
+            if ((date.isSame($scope.from.date, 'day') || date.isAfter($scope.from.date)) &&
+              (date.isSame($scope.to.date, 'day') || date.isBefore($scope.to.date)))
+              totals[key].total++;
+          }
+        });
+      }
+
+      $scope.place.columnTitle = columnTitle;
+      $scope.totals = Object.keys(totals).map(function (key) {
+        return totals[key];
+      });
+    };
 
     ccuBreakdown.all()
       .then(function (rows) {
+        var startState = '';
         $scope.rows = rows
           .filter(function (row) {
             return !!row.facility;
           })
           .map(function (row) {
+            if (!startState.length || row.facility.state < startState)
+              startState = row.facility.state;
+
             return {
               state: row.facility.state,
               zone: row.facility.zone,
@@ -23,6 +134,9 @@ angular.module('lmisApp')
               name: row.name
             };
           });
+
+        $scope.place.search = startState;
+        $scope.updateTotals();
       })
       .catch(function () {
         $scope.error = true;
