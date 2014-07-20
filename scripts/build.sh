439db1281e160e7d33668016203297695d54737e
Tom Vincent <git@tlvince.com>
2014-07-20 16:47:46 +0100

    Travis: build after success

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+info() { echo "$0: $1"; }
+build() { info "Peforming $1 build"; }
+
+# Only build on non-forks
+[[ "TRAVIS_REPO_SLUG" == "eHealthAfrica/LMIS-Dashboard" ]] || exit 1
+
+if [[ "$TRAVIS_TAG" ]]; then
+  build "release"
+  grunt build:release
+else
+  build "snapshot"
+  grunt build
+fi
