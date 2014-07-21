66e8fc8b80e635b792bc4c3a7a2b8e1c1852e484
musamusa <musakunte@gmail.com>
2014-07-21 13:11:10 +0100

    added controller logic for stock count summary

--- app/scripts/controllers/stock_count.js
+++ app/scripts/controllers/stock_count.js
@@ -173,4 +173,72 @@ angular.module('lmisApp')
       .finally(function () {
         $scope.loading = false;
       });
+  })
+  .controller('StockCountSummaryCtrl', function ($scope, stockCountFactory) {
+
+    $scope.facilityStockCounts = {};
+    $scope.toggleAllMode = false;
+    stockCountFactory.stockCountSummaryByFacility()
+      .then(function (data) {
+        $scope.stockCountSummary = data.summary;
+        $scope.groupedStockCount = data.groupedStockCount
+      })
+      .catch(function (reason) {
+        console.error(reason);
+      });
+
+    var setStockCountRowCollapse = function () {
+      $scope.stockCountRowCollapse = {};
+    };
+
+    var getFacilityKeys = function () {
+      return Object.keys($scope.groupedStockCount);
+    };
+
+    var expandAll = function () {
+      setStockCountRowCollapse();
+      var facilityKeys = getFacilityKeys();
+      for (var i = 0; i < facilityKeys.length; i++){
+        $scope.stockCountRowCollapse[facilityKeys[i]] = true;
+        $scope.facilityStockCounts[facilityKeys[i]] = $scope.groupedStockCount[facilityKeys[i]];
+      }
+    };
+
+    var collapseAll = function () {
+      setStockCountRowCollapse();
+      var facilityKeys = getFacilityKeys();
+      for (var i = 0; i < facilityKeys.length; i++){
+        $scope.stockCountRowCollapse[facilityKeys[i]] = false;
+      }
+    };
+
+    $scope.toggleAll = function () {
+      if (!$scope.toggleAllMode) {
+        expandAll();
+        $scope.toggleAllMode = true;
+      } else {
+        collapseAll();
+        $scope.toggleAllMode = false;
+      }
+    };
+
+    setStockCountRowCollapse();
+
+    $scope.toggleRow = function (facilityID) {
+
+      if($scope.stockCountRowCollapse.hasOwnProperty(facilityID)){
+        var currentState = $scope.stockCountRowCollapse[facilityID];
+        setStockCountRowCollapse();
+        $scope.stockCountRowCollapse[facilityID] = !currentState;
+        $scope.toggleAllMode = false;
+      }
+      else{
+        setStockCountRowCollapse();
+        $scope.stockCountRowCollapse[facilityID] = true;
+        $scope.facilityStockCounts[facilityID] = $scope.groupedStockCount[facilityID];
+        $scope.toggleAllMode = false;
+      }
+
+    };
+
   });
