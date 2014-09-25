deadbce175e92f13e121c5b7e11927280254850f
Haykel Ben Jemia <hbj@allmas.tn>
2014-09-25 21:59:09 +0100

    Disable login button while request is running and clear error before every request.

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -1,25 +1,33 @@
 'use strict';
 
 angular.module('lmisApp')
-  .controller('LoginCtrl', function ($scope, Auth, $location) {
+  .controller('LoginCtrl', function($scope, Auth, $location) {
     $scope.user = {};
     $scope.errors = {};
+    $scope.busy = false;
 
     $scope.login = function(form) {
+      $scope.errors.other = null;
       $scope.submitted = true;
 
-      if(form.$valid) {
-        Auth.login({
-          username: $scope.user.username,
-          password: $scope.user.password
-        })
-        .then( function() {
-          // Logged in, redirect to home
-          $location.path('/');
-        })
-        .catch( function(err) {
-          $scope.errors.other = err.message;
-        });
+      if (form.$valid) {
+        $scope.busy = true;
+
+        Auth
+          .login({
+            username: $scope.user.username,
+            password: $scope.user.password
+          })
+          .then(function() {
+            // Logged in, redirect to home
+            $location.path('/');
+          })
+          .catch(function(err) {
+            $scope.errors.other = err.message;
+          })
+          .finally(function() {
+            $scope.busy = false;
+          });
       }
     };
 
