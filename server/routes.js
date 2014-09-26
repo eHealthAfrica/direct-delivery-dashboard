d064a96c21346b8f2620fb582a8ddb9f86f22738
Haykel Ben Jemia <hbj@allmas.tn>
2014-09-26 15:05:21 +0100

    Load uoms from app server.

--- server/routes.js
+++ server/routes.js
@@ -16,15 +16,16 @@ module.exports = function(app) {
   app.use('/api/zones', require('./api/zone'));
   app.use('/api/states', require('./api/state'));
   app.use('/api/cceis', require('./api/ccei'));
+  app.use('/api/uoms', require('./api/uom'));
   app.use('/api/product_categories', require('./api/product_category'));
   app.use('/api/product_presentations', require('./api/product_presentation'));
   app.use('/api/product_profiles', require('./api/product_profile'));
 
   app.use('/auth', require('./auth'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+    .get(errors[404]);
 
   // All other routes should redirect to the index.html
   app.route('/*')
