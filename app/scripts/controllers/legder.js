c1e4a16d7110c02d3712d024a29d3dbce05b9a2f
Musa Musa <musakunte@gmail.com>
2014-09-26 15:26:48 +0100

    added field filters

--- app/scripts/controllers/legder.js
+++ app/scripts/controllers/legder.js
@@ -8,33 +8,61 @@ angular.module('lmisApp')
         controller: 'ledgerCtrl'
       });
   })
-  .controller('ledgerCtrl', function ($scope, $q, ledgerFactory, ngTableParams, $filter) {
-    var data = [];
+  .controller('ledgerCtrl', function ($scope, $q, ledgerFactory, Pagination, $filter) {
+    var rows = [];
+    $scope.filteredRows = [];
+    $scope.search = {};
+    $scope.pagination = new Pagination();
     $scope.loading = true;
     $scope.error = false;
-    $scope.tableModel = {};
 
-    $scope.filterList = function(value) {
-      data = $filter('filter')(data, function(dataObject) {
-        return dataObject.type === value;
-      });
-      console.log(data.length);
+    $scope.from = {
+      opened: false,
+      date: moment().startOf('day').subtract('days', 7).toDate(),
+      open: function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        this.opened = true;
+      }
     };
+
+    $scope.to = {
+      opened: false,
+      date: moment().endOf('day').toDate(),
+      open: function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        this.opened = true;
+      }
+    };
+
+    $scope.$watch('search', function () {
+      updateFilteredRows();
+    }, true);
+
+    function updateFilteredRows() {
+      $scope.filteredRows = $filter('filter')(rows, $scope.search, function (actual, expected) {
+
+        var matches = true;
+        actual = !angular.isDate(actual) && angular.isDefined(actual) ? actual.toLowerCase() : actual;
+        expected = !angular.isDate(expected) && angular.isDefined(expected) ? expected.toLowerCase() : expected;
+
+        if (actual === undefined || actual.indexOf(expected) < 0) {
+          matches = false;
+        }
+
+        return matches;
+      });
+
+      $scope.pagination.totalItemsChanged($scope.filteredRows.length);
+    }
 
     ledgerFactory.getFormattedBundleLines()
       .then(function(response) {
-        data = response;
-
-        $scope.tableParams = new ngTableParams({
-          page: 1,            // show first page
-          count: 10
-        }, {
-          total: data.length, // length of data
-          getData: function($defer, params) {
-            $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-          }
-        });
-
+        rows = response;
+        updateFilteredRows();
         $scope.loading = false;
       })
       .catch(function(reason) {
