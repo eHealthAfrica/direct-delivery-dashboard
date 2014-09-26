5a6b00bb11b95f2cb70457541ab6a3b673c0b39b
Tom Vincent <git@tlvince.com>
2014-09-26 11:40:26 +0100

    Exclude bootstrap-sass from wiredep
    … as it's imported manually in our main.sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -138,7 +138,10 @@ module.exports = function (grunt) {
     // Automatically inject Bower components into the app
     wiredep: {
       app: {
-        src: ['<%= yeoman.app %>/index.html'],
+        src: '<%= yeoman.app %>/index.html',
+        exclude: [
+          'bootstrap-sass-official'
+        ],
         ignorePath: '<%= yeoman.app %>/'
       }
     },
