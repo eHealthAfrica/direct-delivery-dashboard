df5ebea4be497f1e39ee28d22ca7c977ba85e897
Haykel Ben Jemia <hbj@allmas.tn>
2014-09-27 13:21:15 +0100

    Add workaround for an issue in cradle's 'forEach' function.

--- server/app.js
+++ server/app.js
@@ -7,6 +7,31 @@
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+// -----------------------------------------------------------------------
+// fix for cradle's 'forEach' function
+// remove and update cradle when it has been fixed
+//
+var cradleResponse = require('../node_modules/cradle/lib/cradle/response');
+cradleResponse.collectionPrototype.forEach = function(f) {
+  for (var i = 0, value; i < this.length; i++) {
+    value = this[i].doc;
+    if (value === undefined)
+      value = this[i].json;
+    if (value === undefined)
+      value = this[i].value;
+    if (value === undefined)
+      value = this[i];
+
+    if (f.length === 1) {
+      f.call(this[i], value);
+    }
+    else {
+      f.call(this[i], this[i].key, value, this[i].id);
+    }
+  }
+};
+// -----------------------------------------------------------------------
+
 var express = require('express');
 var config = require('./config/environment');
 var cradle = require('cradle');
@@ -21,7 +46,7 @@ require('./config/express')(app);
 require('./routes')(app);
 
 // Start server
-server.listen(config.port, config.ip, function () {
+server.listen(config.port, config.ip, function() {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
