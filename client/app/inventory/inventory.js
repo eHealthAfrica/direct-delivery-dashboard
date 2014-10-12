8adaca75f303366f90f844a22bae9bdfe2aac7d6
Haykel Ben Jemia <hbj@allmas.tn>
2014-10-12 17:57:57 +0100

    Use route resolver for inventory view.

--- client/app/inventory/inventory.js
+++ client/app/inventory/inventory.js
@@ -6,6 +6,62 @@ angular.module('lmisApp')
       .when('/inventory', {
         templateUrl: 'app/inventory/inventory.html',
         controller: 'InventoryCtrl',
-        authenticate: true
+        authenticate: true,
+        resolve: {
+          productTypes: [
+            '$q', '$filter', 'utility', 'ProductType', 'ProductCategory',
+            function($q, $filter, utility, ProductType, ProductCategory) {
+              var d = $q.defer();
+
+              $q.all([
+                  ProductType.all(),
+                  ProductCategory.all()
+                ])
+                .then(function(res) {
+                  var types = res[0];
+                  var categories = res[1];
+
+                  var productTypes = utility.values(types)
+                    .map(function(p) {
+                      var style = categories[p.category];
+                      if (angular.isObject(style)) {
+                        style = ProductCategory.getStyle(style.name);
+                      }
+                      else {
+                        style = '';
+                      }
+                      return {
+                        code: p.code,
+                        style: style
+                      };
+                    });
+
+                  return $filter('orderBy')(productTypes, ['style', 'code']);
+                })
+                .then(d.resolve)
+                .catch(d.reject);
+
+              return d.promise;
+            }
+          ],
+          stockCounts: [
+            '$q', 'stockCount', function($q, stockCount) {
+              var d = $q.defer();
+
+              stockCount.all()
+                .then(stockCount.resolveUnopened)
+                .then(function(rows) {
+                  return {
+                    rows: rows,
+                    latestRows: stockCount.latest(rows)
+                  };
+                })
+                .then(d.resolve)
+                .catch(d.reject);
+
+              return d.promise;
+            }
+          ]
+        }
       });
   });
