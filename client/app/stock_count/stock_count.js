ebc7e8bf7fa06b83b62bf372a2ce572410519e57
Haykel Ben Jemia <hbj@allmas.tn>
2014-10-12 18:18:58 +0100

    Use route resolver for stock count view.

--- client/app/stock_count/stock_count.js
+++ client/app/stock_count/stock_count.js
@@ -6,6 +6,26 @@ angular.module('lmisApp')
       .when('/stock-count', {
         templateUrl: 'app/stock_count/stock_count.html',
         controller: 'StockCountCtrl',
-        authenticate: true
+        authenticate: true,
+        resolve: {
+          productTypes: [
+            'ProductType', function(ProductType) {
+              return ProductType.codes();
+            }
+          ],
+          stockCounts: [
+            '$q', 'stockCount', function($q, stockCount) {
+              var d = $q.defer();
+
+              stockCount.all()
+                .then(stockCount.latest)
+                .then(stockCount.resolveUnopened)
+                .then(d.resolve)
+                .catch(d.reject);
+
+              return d.promise;
+            }
+          ]
+        }
       });
   });
