e3ac75549c3133b12f0683b303809c5d279f04c9
Haykel Ben Jemia <hbj@allmas.tn>
2014-11-02 13:17:18 +0100

    Filter details with summary in CCU breakdown view.

--- client/app/ccu_breakdown/ccu_breakdown.controller.js
+++ client/app/ccu_breakdown/ccu_breakdown.controller.js
@@ -1,88 +1,14 @@
 'use strict';
 
 angular.module('lmisApp')
-  .controller('CCUBreakdownCtrl', function ($scope, $q, $filter, Pagination, Places, CCEI, ccuBreakdown) {
+  .controller('CCUBreakdownCtrl', function($scope, $filter, utility, Auth, Pagination, Places, cceis, ccuBreakdowns) {
+    var rows = ccuBreakdowns;
 
-    $scope.rows = [];
-    $scope.filteredRows = [];
-    $scope.search = {};
-    $scope.pagination = new Pagination();
-    $scope.totals = [];
-    $scope.units = [];
-    $scope.loading = true;
-    $scope.error = false;
-    $scope.places = null;
-
-    $scope.$watch('search', function () {
-      updateFilteredRows();
-    }, true);
-
-    function updateFilteredRows() {
-      $scope.filteredRows = $filter('filter')($scope.rows, $scope.search);
-      $scope.pagination.totalItemsChanged($scope.filteredRows.length);
-    }
-
-    $q.all([
-      CCEI.names(),
-      ccuBreakdown.byDate(),
-      ccuBreakdown.all()
-    ])
-      .then(function (responses) {
-        $scope.loading = true;
-        $scope.units = responses[0];
-        var cceStatus = ['Faulty', 'Fixed'];
-        var cceFaults = ['Leaking', 'Broken Seal', 'No Power Supply', 'Others'];
-        var rows = responses[2];
-        var startState = '';
-        $scope.rows = rows
-          .filter(function (row) {
-            return !!row.facility;
-          })
-          .map(function (row) {
-            if (!startState.length || row.facility.state < startState)
-              startState = row.facility.state;
-
-            return {
-              state: row.facility.state,
-              zone: row.facility.zone,
-              lga: row.facility.lga,
-              ward: row.facility.ward,
-              facility: row.facility.name,
-              created: row.created,
-              name: row.ccuProfile.ModelName,
-              manufacturer: row.ccuProfile.Manufacturer,
-              status: row.ccuStatus.reverse()[0].status,
-              statusText: cceStatus[row.ccuStatus.reverse()[0].status],
-              lastFault: cceFaults[row.ccuStatus.reverse()[0].fault],
-              contact: {
-                name: row.facility.contact.name,
-                email: row.facility.email,
-                phone: row.facility.phone
-              }
-            };
-          });
-
-        //$scope.place.search = startState;
-        //$scope.updateTotals();
-        updateFilteredRows();
-      })
-      .catch(function () {
-        $scope.error = true;
-      })
-      .finally(function () {
-        $scope.loading = false;
-      });
-  });
-  /*.controller('CCUBreakdownCtrl', function($scope, $q, $filter, utility, Auth, Pagination, Places, CCEI, ccuBreakdown) {
     $scope.currentUser = Auth.getCurrentUser();
-    $scope.rows = [];
-    $scope.filteredRows = [];
-    $scope.search = {};
     $scope.pagination = new Pagination();
+    $scope.units = cceis;
+    $scope.filteredRows = [];
     $scope.totals = [];
-    $scope.units = [];
-    $scope.loading = true;
-    $scope.error = false;
     $scope.places = null;
 
     $scope.place = {
@@ -119,14 +45,15 @@ angular.module('lmisApp')
       return $scope.places.promise;
     };
 
-    $scope.updateTotals = function() {
+    $scope.update = function() {
       var totals = {};
       var filterBy = Places.propertyName($scope.place.type);
       var subType = $scope.place.type === Places.FACILITY ? Places.FACILITY : Places.subType($scope.place.type);
       var groupBy = Places.propertyName(subType || $scope.currentUser.access.level);
       var columnTitle = Places.typeName(subType || $scope.currentUser.access.level);
 
-      utility.placeDateFilter($scope.rows, filterBy, $scope.place.search, $scope.from.date, $scope.to.date).forEach(function(row) {
+      $scope.filteredRows = utility.placeDateFilter(rows, filterBy, $scope.place.search, $scope.from.date, $scope.to.date);
+      $scope.filteredRows.forEach(function(row) {
         var key = row.facility[groupBy];
         totals[key] = totals[key] || {
           place: key,
@@ -147,37 +74,9 @@ angular.module('lmisApp')
           })
         };
       });
+
+      $scope.pagination.totalItemsChanged($scope.filteredRows.length);
     };
 
-    $scope.$watch('search', function() {
-      updateFilteredRows();
-    }, true);
-
-    function updateFilteredRows() {
-      $scope.filteredRows = $filter('filter')($scope.rows, $scope.search, utility.objectComparator);
-      $scope.pagination.totalItemsChanged($scope.filteredRows.length);
-    }
-
-    $q.all([
-        CCEI.names(),
-        ccuBreakdown.byDate()
-      ])
-      .then(function(responses) {
-        $scope.units = responses[0];
-
-        var rows = responses[1];
-        $scope.rows = rows
-          .filter(function(row) {
-            return !!row.facility;
-          });
-
-        $scope.updateTotals();
-        updateFilteredRows();
-      })
-      .catch(function() {
-        $scope.error = true;
-      })
-      .finally(function() {
-        $scope.loading = false;
-      });
-  });*/
+    $scope.update();
+  });
