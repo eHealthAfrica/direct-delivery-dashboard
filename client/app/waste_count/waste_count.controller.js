5227b200ccb84fb21bbabd6c9b67e951723bd468
Haykel Ben Jemia <hbj@allmas.tn>
2014-11-02 15:16:39 +0100

    Filter details with summary in waste count view.

--- client/app/waste_count/waste_count.controller.js
+++ client/app/waste_count/waste_count.controller.js
@@ -8,10 +8,8 @@ angular.module('lmisApp')
     $scope.pagination = new Pagination();
     $scope.productTypes = productTypes;
     $scope.filteredRows = [];
-    $scope.search = {};
     $scope.totals = [];
     $scope.places = null;
-    $scope.showDetails = false;
 
     $scope.csvHeader = [
       's/n',
@@ -25,22 +23,6 @@ angular.module('lmisApp')
       'Reason',
       'Quantity'
     ];
-
-    $scope.showDetail = function(place) {
-      var prop = Places.propertyName($scope.place.columnTitle.toLowerCase());
-
-      if (place === undefined) {
-        $scope.showDetails = false;
-      }
-      else if ($scope.showDetails && $scope.search.facility[prop] === place) {
-        $scope.showDetails = false;
-      }
-      else {
-        $scope.search.facility = {};
-        $scope.search.facility[prop] = place;
-        $scope.showDetails = true;
-      }
-    };
 
     $scope.place = {
       type: '',
@@ -70,40 +52,21 @@ angular.module('lmisApp')
       }
     };
 
-    $scope.created = {
-      opened: false,
-      date: moment().endOf('day').toDate(),
-      open: function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-
-        this.opened = true;
-      }
-    };
-
     $scope.getPlaces = function(filter) {
       $scope.places = new Places($scope.place.type, filter);
 
       return $scope.places.promise;
     };
 
-    $scope.updateTotals = function() {
+    $scope.update = function() {
       var totals = {};
       var filterBy = Places.propertyName($scope.place.type);
       var subType = $scope.place.type === Places.FACILITY ? Places.FACILITY : Places.subType($scope.place.type);
       var groupBy = Places.propertyName(subType || $scope.currentUser.access.level);
       var columnTitle = Places.typeName(subType || $scope.currentUser.access.level);
 
-      if ($scope.place.search.length) {
-        $scope.search = {};
-        if (angular.isUndefined($scope.search.facility)) {
-          $scope.search.facility = {};
-        }
-        $scope.search.facility[filterBy] = $scope.place.search;
-        $scope.search.created = $scope.from.date;
-      }
-
-      utility.placeDateFilter(rows, filterBy, $scope.place.search, $scope.from.date, $scope.to.date).forEach(function(row) {
+      $scope.filteredRows = utility.placeDateFilter(rows, filterBy, $scope.place.search, $scope.from.date, $scope.to.date);
+      $scope.filteredRows.forEach(function(row) {
         var key = row.facility[groupBy];
         totals[key] = totals[key] || {
           place: key,
@@ -126,48 +89,9 @@ angular.module('lmisApp')
           })
         };
       });
+
+      $scope.pagination.totalItemsChanged($scope.filteredRows.length);
     };
 
-    $scope.$watch('search', function() {
-      updateFilteredRows();
-    }, true);
-
-    function updateFilteredRows() {
-      $scope.filteredRows = $filter('filter')(rows, $scope.search, function(actual, expected) {
-        var matches = true;
-        Object.keys(expected).some(function(key) {
-          if (angular.isArray(actual)) {
-            actual.some(function(actualKey) {
-              if (actualKey[key] === undefined || actualKey[key].toLowerCase().indexOf(expected[key].toLowerCase()) < 0) {
-                matches = false;
-                return true;
-              }
-            });
-          }
-          else if (actual[key] === undefined || actual[key].toLowerCase().indexOf(expected[key].toLowerCase()) < 0) {
-
-            matches = false;
-            return true;
-          }
-          return false;
-        });
-
-        if (angular.isDate(actual) || angular.isDate(expected)) {
-          var date = moment(Date.parse(actual));
-          if ($scope.place.search.length) {
-            matches = ((date.isSame($scope.from.date, 'day') || date.isAfter($scope.from.date)) &&
-                       (date.isSame($scope.to.date, 'day') || date.isBefore($scope.to.date)));
-          }
-          else {
-            matches = date.isSame(expected, 'day');
-          }
-        }
-
-        return matches;
-      });
-      $scope.pagination.totalItemsChanged($scope.filteredRows.length);
-    }
-
-    $scope.updateTotals();
-    updateFilteredRows();
+    $scope.update();
   });
