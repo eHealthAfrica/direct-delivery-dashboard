c94afeb4e5636548b5545c7a889279a5e06caccc
femi oni <freefony@gmail.com>
2014-11-10 10:59:55 +0100

    pie chart for ccu breakdowns

--- client/app/ccu_breakdown/ccu_breakdown.controller.js
+++ client/app/ccu_breakdown/ccu_breakdown.controller.js
@@ -3,11 +3,12 @@
 angular.module('lmisApp')
   .controller('CCUBreakdownCtrl', function($scope, $filter, utility, Auth, Pagination, Places, ccuBreakdowns) {
     var rows = ccuBreakdowns;
-
     $scope.currentUser = Auth.getCurrentUser();
     $scope.pagination = new Pagination();
     $scope.filteredRows = [];
     $scope.places = null;
+    $scope.broken = 0;
+    $scope.fixed  = 0;
 
     $scope.place = {
       type: '',
@@ -32,7 +33,6 @@ angular.module('lmisApp')
       open: function($event) {
         $event.preventDefault();
         $event.stopPropagation();
-
         this.opened = true;
       }
     };
@@ -48,7 +48,38 @@ angular.module('lmisApp')
 
       $scope.filteredRows = utility.placeDateFilter(rows, filterBy, $scope.place.search, $scope.from.date, $scope.to.date);
       $scope.pagination.totalItemsChanged($scope.filteredRows.length);
+      setChartData($scope.filteredRows);
     };
 
     $scope.update();
-  });
+    function updateChartData(){
+      $scope.breakdownChartData = [
+      { key: "Broken", y: $scope.broken, color:'#f33' },
+      { key: "Fixed", y: $scope.fixed, color:'#3f3' }
+      ];
+      console.log($scope.broken + "-" + $scope.fixed);
+    }
+    function setChartData(rows){
+       rows.forEach(function(row){
+          if(row.status === 1){
+            $scope.fixed = $scope.fixed + 1;
+
+          }else {
+            $scope.broken = $scope.broken + 1;
+          }
+
+      })
+      console.log($scope.broken);
+      updateChartData();
+    }
+      $scope.xFunction = function(){
+        return function(d) {
+          return d.key;
+        };
+      }
+      $scope.yFunction = function(){
+        return function(d){
+          return d.y;
+        };
+      }
+ });
