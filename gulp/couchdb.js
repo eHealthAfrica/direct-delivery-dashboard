2059efdd013dd1057f39886c29f98b4f339767c1
Tom Vincent <git@tlvince.com>
2015-01-21 12:05:11 +0000

    Add S3 deployment

--- gulp/couchdb.js
+++ gulp/couchdb.js
@@ -4,22 +4,9 @@ var got = require('got');
 var glob = require('glob');
 var gulp = require('gulp');
 var compile = require('couch-compile');
-var dataModel = require('data-model');
 
 var config = require('../config');
 var fixtures = require('../couchdb/fixtures');
-
-var models = [
-  'driver'
-];
-
-function instances(factory) {
-  var all = [];
-  for (var model in factory) {
-    all = all.concat(factory[model]);
-  }
-  return all;
-}
 
 function push(docs) {
   var url = config.config.db + '/_bulk_docs';
@@ -44,30 +31,10 @@ function push(docs) {
   });
 }
 
-gulp.task('fixtures-generated', function() {
-  var count = process.argv[4] || 10;
-  var factory = dataModel.generate(models, count);
-  var docs = instances(factory);
-  return push(docs);
-});
-
 gulp.task('fixtures-local', function() {
   for (var model in fixtures) {
     var docs = fixtures[model].docs;
     push(docs);
-  }
-});
-
-gulp.task('fixtures-validate', function() {
-  for (var model in fixtures) {
-    var docs = fixtures[model].docs;
-    docs.forEach(function(instance) {
-      var errors = dataModel.validate(instance);
-      if (errors) {
-        errors = JSON.stringify(errors, null, 2);
-        throw new Error('"' + model + '" fixtures are invalid:\n' + errors);
-      }
-    });
   }
 });
 
@@ -90,9 +57,3 @@ gulp.task('views', function() {
     matches.forEach(couchCompile);
   });
 });
-
-gulp.task('fixtures', [
-  'fixtures-generated',
-  'fixtures-validate',
-  'fixtures-local'
-]);
