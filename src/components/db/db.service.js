8ae321fff88b99a1a6878eb52409b8c2d82506e7
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-06-16 17:01:27 +0100

    add DB module

---
+++ src/components/db/db.service.js
@@ -0,0 +1,101 @@
+'use strict';
+
+/**
+ * @name storageService
+ * @desc
+ */
+angular.module('db')
+  .service('dbService', function(config, pouchDB) {
+
+    var _this = this;
+    var remoteDB = pouchDB(config.db);
+
+    _this.addTimeInfo = function (doc) {
+      var now = new Date().toJSON();
+      if (!doc.createdOn) {
+        doc.createdOn = now;
+      }
+      doc.modifiedOn = now;
+      return doc;
+    };
+
+    /**
+     * This does the following:
+     * 1. Update an existing document.
+     * 2. Insert a new document that has _id property.
+     * 3. Insert a new document that does not have _id property and assign it an _id.
+     *
+     * @param {Object} doc - document to be saved.
+     * @returns {$promise}
+     */
+    _this.save = function (doc) {
+      doc = _this.addTimeInfo(doc);
+      if (doc._id) {
+        return _this.update(doc)
+          .catch(function () {
+            return remoteDB.put(doc, doc._id)
+              .then(function (res) {
+                doc._id = res.id;
+                doc._rev = res.rev;
+                return doc;
+              });
+          });
+      } else {
+        return _this.insert(doc);
+      }
+    };
+
+    _this.get = function (id) {
+      return remoteDB.get(id);
+    };
+
+    _this.delete = function (doc) {
+      return remoteDB.get(doc._id)
+          .then(function(doc){
+            return remoteDB.remove(doc);
+          });
+    };
+
+    /**
+     * Used to save a new document without an _id property, Pouchdb or
+     * underlying service should generate the _id.
+     *
+     * @param doc - a new document without an _id.
+     * @returns {$promise}
+     * @see http://pouchdb.com/api.html#create_document
+     */
+    _this.insert = function (doc) {
+      doc = _this.addTimeInfo(doc);
+      return remoteDB.post(doc)
+        .then(function (res) {
+          doc._id = res.id;
+          doc._rev = res.rev;
+          return doc;
+        });
+    };
+
+    /**
+     * Updates document only if it exists.
+     *
+     * @param doc - existing document with _id property.
+     * @returns {$promise}
+     */
+    _this.update = function (doc) {
+      doc = _this.addTimeInfo(doc);
+      return remoteDB.get(doc._id)
+        .then(function (res) {
+          doc._rev = res._rev;
+          return remoteDB.put(doc, doc._id)
+            .then(function (res) {
+              doc._id = res.id;
+              doc._rev = res.rev;
+              return doc;
+            });
+        });
+    };
+
+    _this.getView = function(view, options){
+      return remoteDB.query(view, options);
+    };
+
+  });
