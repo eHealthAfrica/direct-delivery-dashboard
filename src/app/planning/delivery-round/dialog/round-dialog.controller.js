9d37d1a368d579f971a56de933d3e6bc2b4d25e4
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-06-17 17:06:26 +0100

    complete creation and editing of delivery round without validations (to be added after workflow has been approved)

--- src/app/planning/delivery-round/dialog/round-dialog.controller.js
+++ src/app/planning/delivery-round/dialog/round-dialog.controller.js
@@ -1,14 +1,21 @@
 angular.module('planning')
-		.controller('RoundDialogCtrl', function ($scope, $modalInstance, config) {
+		.controller('RoundDialogCtrl', function ($scope, log, $modalInstance, config, deliveryRound,
+                                             $state, planningService) {
 
 			var vm = this;//view model
+      vm.edit = false;
 
-			vm.deliveryRound = {
-				state: '',
-				roundNo: '',
-				startDate: new Date(),
-				endDate: ''
-			};
+      if(!angular.isObject(deliveryRound)){
+	      vm.deliveryRound = {
+		      state: '',
+		      roundNo: '',
+		      startDate: new Date(),
+		      endDate: ''
+	      };
+      }else{
+	      vm.deliveryRound = deliveryRound;
+	      vm.edit = true;
+      }
 
 			//TODO: load this from DB when location and facility has been pulled in.
 			vm.states = [
@@ -16,37 +23,89 @@ angular.module('planning')
 				{ name: "Bauchi", code: 'BA'}
 			];
 
+			function openDatePicker($event) {
+				$event.preventDefault();
+				$event.stopPropagation();
+				this.opened = true;
+			}
 
 			$scope.dateFormat = config.dateFormat;
 			$scope.start = {
 				opened: false,
-				open: function($event) {
-					$event.preventDefault();
-					$event.stopPropagation();
-					this.opened = true;
+				open: openDatePicker
+			};
+
+			vm.setRoundNumber = function(){
+				if(angular.isString(vm.deliveryRound._id) && !vm.deliveryRound.roundNo){
+					var strList = vm.deliveryRound._id.split('-');
+					var roundNoIndex = 1;
+					vm.deliveryRound.roundNo = strList[roundNoIndex];
 				}
 			};
 
+			vm.setRoundNumber();
+
 			vm.getRoundCode = function(){
-				var startYear = new Date(vm.deliveryRound.startDate).getFullYear();
-				return [
-					vm.deliveryRound.state,
-					vm.deliveryRound.roundNo,
-					startYear
-				].join('-');
+				return planningService.getRoundCode(vm.deliveryRound);
 			};
 
 			$scope.end = {
 				opened: false,
-				open: function($event) {
-					$event.preventDefault();
-					$event.stopPropagation();
-					this.opened = true;
+				open: openDatePicker
+			};
+
+			function onSuccessContinue(res){
+				log.success('savedDeliveryRound');
+				return $modalInstance.close(res);
+				//TODO: $state.go('schedulePage');
+			}
+
+			function onSuccessExit(res){
+				log.success('savedDeliveryRound');
+				$state.go('planning.deliveryRound', $state.params, { reload: true })
+						.finally(function(){
+							$modalInstance.close(res);
+						});
+			}
+
+			function createAndContinue(){
+				planningService.createRound(vm.deliveryRound)
+						.then(onSuccessContinue)
+						.catch(planningService.onSaveError);
+			}
+
+			function saveEditAndContinue(){
+				planningService.saveRound(vm.deliveryRound)
+						.then(onSuccessContinue)
+						.catch(planningService.onSaveError);
+			}
+
+			function createAndExit(){
+				planningService.createRound(vm.deliveryRound)
+						.then(onSuccessExit)
+						.catch(planningService.onSaveError);
+			}
+
+			function saveEditAndExit(){
+				planningService.saveRound(vm.deliveryRound)
+						.then(onSuccessExit)
+						.catch(planningService.onSaveError);
+			}
+
+			vm.continue = function(){
+				if(vm.edit){
+					saveEditAndContinue();
+				}else{
+					createAndContinue();
 				}
 			};
 
-			$scope.continue = function () {
-				$modalInstance.close($scope.selected.item);
+			vm.saveAndExit = function(){
+				if(vm.edit){
+					saveEditAndExit();
+				}else{
+					createAndExit();
+				}
 			};
 
 			vm.cancel = function () {
