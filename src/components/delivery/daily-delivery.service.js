d976e8635d471e0d65bc46db5f8b7c6dd60aba0e
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-06-17 17:08:10 +0100

    started implementation of delivery service, this might be replaced by a shared external module

---
+++ src/components/delivery/daily-delivery.service.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('delivery', [])
+		.service('deliveryService', function(dbService, pouchUtil){
+
+			this.getByRoundId = function(roundId) {
+				var view = 'daily-deliveries/by-round';
+				var params = {};
+				if (angular.isString(roundId) || angular.isArray(roundId)) {
+					params = pouchUtil.key(roundId);
+				}
+				/*eslint-disable camelcase */
+				params.include_docs = true;
+				/*eslint-enable camelcase */
+
+				return dbService.getView(view, params)
+						.then(pouchUtil.pluckDocs)
+						.then(pouchUtil.rejectIfEmpty);
+			};
+
+		});
