d2b2dbc79874b6267e8330e2e8c3ee2e6aea6865
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-06-19 16:04:32 +0100

    add delivery round selection handlers

--- src/app/planning/facilities/dialogs/copy-round/copy-round-dialog.controller.js
+++ src/app/planning/facilities/dialogs/copy-round/copy-round-dialog.controller.js
@@ -1,12 +1,33 @@
 angular.module('planning')
-		.controller('CopyRoundTemplateDialogCtrl', function($modalInstance, deliveryRounds) {
+		.controller('CopyRoundTemplateDialogCtrl', function($modalInstance, deliveryService, $modalInstance,
+                                                        deliveryRound, deliveryRounds, log, copyRoundService) {
 			var vm = this;
 
 			vm.selectedRoundId = '';
 			vm.deliveryRounds = deliveryRounds;
+			vm.isLoading = false;
+			vm.currentRound = deliveryRound;
 
 			vm.cancel = function () {
 				$modalInstance.dismiss('cancel');
+			};
+
+			function onSuccess(dailySchedules){
+				var roundTemplate = copyRoundService.prepareFromTemplate(vm.currentRound._id, dailySchedules);
+				$modalInstance.close(roundTemplate);
+				vm.isLoading = true;
+			}
+
+			function onError(err){
+				log.error('deliveryRoundDoesNotHaveDailySchedule', err);
+				vm.isLoading = false;
+			}
+
+			vm.copy = function(){
+				vm.isLoading = true;
+				deliveryService.getByRoundId(vm.selectedRoundId)
+						.then(onSuccess)
+						.catch(onError);
 			};
 
 		});
