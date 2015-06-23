bcd099014dbfc7bf26cd24016f1b7fc15ae60f20
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-06-23 15:56:59 +0100

    add temp function for flattening daily delivery doc until doc split

---
+++ src/app/planning/schedule/schedules.service.js
@@ -0,0 +1,34 @@
+angular.module('planning')
+		.service('scheduleService', function (dbService) {
+
+			var _this = this;
+
+			_this.saveSchedules = function(schedules){
+				return dbService.saveDocs(schedules);
+			};
+
+			_this.flatten = function(dailyDeliveries) {
+				var schedules = [];
+				///TODO: Remove after splitting Daily Delivery doc into
+				function flattenSchedule(dailySchedule) {
+					if(angular.isArray(dailySchedule.facilityRounds)){
+						dailySchedule.facilityRounds
+								.forEach(function(facRnd){
+									var schedule = {
+										facility: facRnd.facility,
+										date: dailySchedule.date,
+										driverID: dailySchedule.driverID,
+										drop: facRnd.drop,
+										window: facRnd.window,
+										status: facRnd.status
+									};
+									schedules.push(schedule);
+								});
+					}
+				}
+
+				dailyDeliveries.forEach(flattenSchedule);
+				return schedules;
+			};
+
+		});
