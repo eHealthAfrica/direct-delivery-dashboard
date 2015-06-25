4f7f624ecfd9d614073048749692fda054060106
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-06-25 16:10:34 +0100

    mock schedule controller

--- src/app/planning/schedule/schedule-round.controller.js
+++ src/app/planning/schedule/schedule-round.controller.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('planning')
-		.controller('ScheduleRoundCtrl', function (deliveryRound, drivers, dailyDeliveries, scheduleService) {
+		.controller('ScheduleRoundCtrl', function (deliveryRound, dailyDeliveries, scheduleService) {
 
 			var vm = this;
 			vm.deliveryRound = deliveryRound;
 			vm.dailyDeliveries = scheduleService.flatten(dailyDeliveries);
-      vm.drivers = drivers;
+      vm.drivers = [];
 
 
 		});
