6293d94fc0c22b22380f3f4fe7711d75b1ef024d
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-06-30 15:52:17 +0100

    add message for empty facility list

--- src/components/log/messages/info.constants.js
+++ src/components/log/messages/info.constants.js
@@ -2,4 +2,8 @@
 
 angular.module('log')
   .constant('INFO_MESSAGES', {
+      noFacilityInAdminLevels: {
+        title: 'Empty facility list',
+        message: 'Please select different admin levels or upload facilities for selected levels'
+      }
   });
