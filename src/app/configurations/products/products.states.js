3349f4f5ad68333892f09112df530952cc0de935
femi oni <freefony@gmail.com>
2015-07-08 10:22:53 +0100

    product module prototype

---
+++ src/app/configurations/products/products.states.js
@@ -0,0 +1,20 @@
+/**
+ * Created by ehealthafrica on 7/7/15.
+ */
+
+angular.module('products')
+  .config(function($stateProvider){
+    $stateProvider
+      .state('configurations.products', {
+        parent: 'configurations.layout',
+        url: '/products',
+        templateUrl: 'app/configurations/products/product.html',
+        controller: 'ProductsController',
+        controllerAs: 'productsController',
+        resolve: {
+          products : function(productService){
+            return productService.getAll();
+          }
+        }
+      })
+  });
