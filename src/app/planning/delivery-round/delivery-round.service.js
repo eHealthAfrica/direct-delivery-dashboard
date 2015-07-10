4bf3913c767fde81aeeb0f31fd8cd530dd50a652
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-07-10 16:10:05 +0100

    add delivery round report function for fetching report from db

--- src/app/planning/delivery-round/delivery-round.service.js
+++ src/app/planning/delivery-round/delivery-round.service.js
@@ -1,4 +1,21 @@
 angular.module('planning')
-		.service('deliveryRoundService', function (dbService, pouchUtil, log, ROUND_STATUS) {
+		.service('deliveryRoundService', function (dbService, pouchUtil) {
+
+			var _this = this;
+
+			_this.getReport = function(roundId) {
+				var view = 'dashboard-delivery-rounds/report-by-id';
+        var params = {
+	        key: roundId,
+	        reduce: true
+        };
+				return dbService.getView(view, params)
+						.then(function(res){
+							if(res.rows.length === 0){
+								return pouchUtil.rejectIfEmpty(res.rows);
+							}
+							return res.rows[0].value;
+						});
+			};
 
 		});
