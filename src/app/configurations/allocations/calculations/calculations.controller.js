c71cededc67e35bef484a80a987850ad62dacd97
femi oni <freefony@gmail.com>
2015-07-13 12:40:56 +0100

    add target population method to calculation  service

--- src/app/configurations/allocations/calculations/calculations.controller.js
+++ src/app/configurations/allocations/calculations/calculations.controller.js
@@ -12,24 +12,47 @@ angular.module('allocations')
     vm.renderedData = [];
     vm.locationStates = ['KN', 'BA'];
     vm.selectedState = 'KN';
+    vm.selectedLga = '';
     vm.lgas = [];
     vm.wards = [];
 
     vm.findLga = function(state){
-      locationService.getByIds([state])
+      var keys = [];
+      keys.push(["4", state]);
+      return locationService.getByLevelAndAncestor(keys)
         .then(function(response){
           vm.lgas = response;
+          return vm.lgas;
         })
         .catch(function(err){
           log.error('', err, 'could not fetch lga list, please try again. contact admin if this persists.');
         });
     };
-    vm.findWard = function(lga){
 
-    };
-    vm.filterLocation = function(level, id){
+    vm.filterByLocation = function(locationid){
+      var level = "6";
+      var keys = [];
 
+      if(angular.isArray(locationid)){
+       for(i in locationid){
+         keys.push([level, locationid[i]._id]);
+       }
+      }else{
+        keys.push([level, locationid._id]);
+      }
+      return locationService.getByLevelAndAncestor(keys)
+        .then(function(response){
+          return response;
+        })
     };
+    vm.findLga(vm.selectedState)
+      .then(function(lgas){
+        vm.selectedLga = lgas[0];
+        return vm.selectedLga;
+      })
+      .then(vm.filterByLocation)
+      .then(calculationService.getTargetPop);
+
     function  switchRenderedPartial (partial){
       return vm.renderedPartial = partial;
     }
@@ -37,9 +60,6 @@ angular.module('allocations')
       return vm.renderedData;
     }
 
-    vm.filterLocation = function(location){
-
-    };
     vm.changeDataView = function(partial, viewLabel){
       vm.renderedViewLabel = viewLabel;
       switchRenderedPartial(partial);
--- src/app/configurations/allocations/calculations/calculations.service.js
+++ src/app/configurations/allocations/calculations/calculations.service.js
@@ -3,10 +3,10 @@
  */
 
 angular.module('allocations')
-  .service('calculationService', function(locationService){
+  .service('calculationService', function(locationService, dbService){
 
     var service = this;
-
+    //TODO: change all static level reference to dynamic types
     service.getCurrentTemplate = function(){
 
     };
@@ -14,13 +14,37 @@ angular.module('allocations')
       return locationService.get
     };
     service.getfacility = function(){
-      return locationService.getLocationByLevel(6);
+      return locationService.getLocationByLevel("6");
     };
     service.saveCustomAllocation = function(){
 
     };
 
-    service.getTargetPop = function(keys){
+    /**
+     * gets target population data for facilities
+     * @param facilities. object or array of facility objects, preferably from location service
+     * @return facilities.  match each facility entered with corresponding returned
+     * data.
+     */
+    service.getTargetPop = function(facilities){
+      var view = 'alloccations/target_population',
+        keys = [],
+        options = {
+        include_docs: true
+      };
 
+      if(angular.isArray(facilities)){
+        for($i in facilities){
+          keys.push(facilities[$i]._id);
+        }
+      }else{
+       keys.push(facilities._id);
+      }
+      options.keys = keys;
+      return dbService.getView(view, options)
+        .then(function(response){
+          console.log(response);
+          return response;
+        });
     }
 });
