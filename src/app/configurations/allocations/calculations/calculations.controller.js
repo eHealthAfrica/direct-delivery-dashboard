1c8652c65756d56a11d11378fa34c29c07e98d8e
femi oni <freefony@gmail.com>
2015-07-14 10:56:34 +0100

    reset view on user action

--- src/app/configurations/allocations/calculations/calculations.controller.js
+++ src/app/configurations/allocations/calculations/calculations.controller.js
@@ -3,12 +3,18 @@
  */
 
 angular.module('allocations')
-  .controller('CalculationsController', function(products, locations, locationService, calculationService, pouchUtil, log){
+  .controller('CalculationsController', function (products, locations, locationService, calculationService, pouchUtil, log) {
     var vm = this;
-
+    var viewMap = {
+      targetPop: "getTargetPop",
+      coverage: "computeCoverage",
+      wastage: "computeWastage",
+      schedule: "computeSchedule"
+    };
     vm.productList = products || [];
     vm.renderedPartial = 'tp';
-    vm.renderedViewLabel = 'target populations';
+    vm.renderedViewLabel = 'target populations'; 
+    vm.activeView = 'targetPop';
     vm.renderedData = [];
     vm.locationStates = ['KN', 'BA'];
     vm.selectedState = 'KN';
@@ -16,58 +22,81 @@ angular.module('allocations')
     vm.lgas = [];
     vm.wards = [];
 
-    vm.findLga = function(state){
+    findLga = function (state) {
       var keys = [];
       keys.push(["4", state]);
       return locationService.getByLevelAndAncestor(keys)
-        .then(function(response){
+        .then(function (response) {
           vm.lgas = response;
-          return vm.lgas;
+          vm.selectedLga = response[0];
+          return vm.selectedLga;
         })
-        .catch(function(err){
+        .catch(function (err) {
           log.error('', err, 'could not fetch lga list, please try again. contact admin if this persists.');
         });
     };
+    function resetView (facilities) {
+      return calculationService[viewMap[vm.activeView]](facilities)
+        .then(function (response) {
+          vm.renderedData = response;
+          return response;
+        });
+    }
 
-    vm.filterByLocation = function(locationid){
+    function getFacilities(lgs) {
       var level = "6";
       var keys = [];
-
-      if(angular.isArray(locationid)){
-       for(i in locationid){
-         keys.push([level, locationid[i]._id]);
-       }
-      }else{
-        keys.push([level, locationid._id]);
+      var lgas = lgs || vm.selectedLga;
+      if(!lgas){
+        vm.rederedData = [];
+        return;
+      }
+      if (angular.isArray(lgas)) {
+        for (i in locationid) {
+          keys.push([level, lgas[i]._id]);
+        }
+      } else {
+        keys.push([level, lgas._id]);
       }
       return locationService.getByLevelAndAncestor(keys)
-        .then(function(response){
+        .then(function (response) {
           return response;
         })
+        .catch(function(err){
+          log.error('','', 'could not retrieve facilitie, please reload and try again')
+        })
+    }
+    function switchRenderedPartial(partial) {
+      vm.renderedPartial = partial;
+      return getFacilities()
+        .then(resetView)
+    }
+    vm.switchLocationState = function (stateID) {
+      var state = stateID || vm.selectedState;
+      return findLga(state)
+        .then(getFacilities)
+        .then(resetView)
+        .catch(function(err){
+          log.error('',err, 'switching LGA failed');
+          return [];
+        })
     };
-    vm.findLga(vm.selectedState)
-      .then(function(lgas){
-        vm.selectedLga = lgas[0];
-        return vm.selectedLga;
-      })
-      .then(vm.filterByLocation)
-      .then(calculationService.getTargetPop)
-      .then(function(response){
-        console.log(response);
-        vm.renderedData = response;
+    //TODO: accomodate more than one lga at a time; input to single lga or array of lgas
+    vm.switchLocationLga = function (lgas) {
+     vm.selectedLga = lgas;
+     return getFacilities(lgas)
+      .then(resetView)
+      .catch(function (err) {
+        log.error('', '', 'switching LGA failed please reload page and try again')
       });
+    };
 
-    function  switchRenderedPartial (partial){
-      return vm.renderedPartial = partial;
-    }
-    function switchRenderedData (view){
-      return vm.renderedData;
-    }
+    vm.switchLocationState(vm.selectedState);
 
-    vm.changeDataView = function(partial, viewLabel){
+
+    vm.changeDataView = function (partial, viewLabel) {
       vm.renderedViewLabel = viewLabel;
       switchRenderedPartial(partial);
-      switchRenderedData(viewLabel.replace(' ', ''));
     };
 
   });
