d56cf3fb3f862a27549c5bb94996d526b01d041c
femi oni <freefony@gmail.com>
2015-07-15 11:50:18 +0100

    template service

---
+++ src/app/configurations/allocations/assumptions/assumptions.service.js
@@ -0,0 +1,18 @@
+/**
+ * Created by ehealthafrica on 7/15/15.
+ */
+
+angular.module('allocations')
+  .service('templateService', function(log){
+
+    var service = this;
+
+    service.setTemplate = function(){
+
+    };
+
+    service.getTemplate = function(){
+
+    };
+
+  });
