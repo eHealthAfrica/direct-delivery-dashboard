f753aa66c462c8371b49e2822bc6bd8307130dbe
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-07-28 16:00:24 +0100

    replace temp view with permanent view

--- server/api/app_config/app_config.model.js
+++ server/api/app_config/app_config.model.js
@@ -8,23 +8,10 @@ var db = new (cradle.Connection)().database('app_config');
 exports.get = get;
 
 function get(cb) {
-  db.temporaryView({
-    map: function(doc) {
-      emit(doc.id, {
-        _id: doc._id,
-        uuid: doc.uuid,
-        rev: doc._rev,
-        facility: {
-          _id: doc.facility._id,
-          name: doc.facility.name,
-          reminderDay: doc.facility.reminderDay,
-          stockCountInterval: doc.facility.stockCountInterval
-        }
-      });
+  db.view('app_config/by_id', function(err, rows) {
+    if (err){
+      return cb(err);
     }
-  }, function(err, rows) {
-    if (err) return cb(err);
-
     cb(null, rows.toArray());
   });
 }
