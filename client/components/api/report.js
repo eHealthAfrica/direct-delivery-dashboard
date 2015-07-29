55bec6f0dc59e146f6170e26e6b0afb83ec2b737
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-07-29 23:09:31 +0100

    parse report data to chart data format

--- client/components/api/report.js
+++ client/components/api/report.js
@@ -15,7 +15,32 @@ angular.module('lmisApp')
 			_this.getWithin = function(startDate, endDate){
 				return $http.get(URL)
 						.then(function(res){
-							return res.data;
+							var report = res.data;
+							var chartData = [
+								{
+									"key": "Functional CCE",
+									"color": "#72BCD4",
+									"values": []
+								},
+								{
+									"key": "Stocked To Plan",
+									"color": "#66B266",
+									"values": []
+								},
+								{
+									"key": "Reporting",
+									"color": "#937e83",
+									"values": []
+								}
+							];
+
+							for(var zone in report.activeZones){
+								chartData[0].values.push([ zone, report.cceBreakdown[zone] ]);
+								chartData[1].values.push([ zone, 52 ]);//TODO: replace with stock to plan when completed
+								chartData[2].values.push([ zone, report.reporting[zone] ]);
+							}
+
+							return chartData;
 						});
 			};
 
