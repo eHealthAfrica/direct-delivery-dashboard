0578bcb128dc02ab66b5020e23240de56a853c88
Musa Musa <musakunte@gmail.com>
2015-08-03 10:39:47 +0100

    added methods to get/update appConfig

--- server/api/app_config/index.js
+++ server/api/app_config/index.js
@@ -7,5 +7,7 @@ var auth = require('../../auth/auth.service');
 var router = express.Router();
 
 router.get('/', auth.isAuthenticated(), controller.index);
-
+router.get('/all', auth.isAuthenticated(), controller.all);
+router.get('/:id', auth.isAuthenticated(), controller.get);
+router.put('/:id', auth.isAuthenticated(), controller.put);
 module.exports = router;
