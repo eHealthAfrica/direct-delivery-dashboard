121dcce22e5e2d8a8a48b7f882a36ae1fd5db4fc
Musa Musa <musakunte@gmail.com>
2015-08-03 10:49:11 +0100

    added phone status update

--- client/app/facilities/facilities.controller.js
+++ client/app/facilities/facilities.controller.js
@@ -1,10 +1,38 @@
 'use strict';
 
 angular.module('lmisApp')
-  .controller('FacilitiesCtrl', function($q, Auth, facilityCSV, facilityChart, FACILITY_FILTERS, summaries) {
+  .controller('FacilitiesCtrl', function($q, Auth, facilityCSV, facilityChart, FACILITY_FILTERS, summaries, $scope, AppConfig) {
     var vm = this;
     vm.currentUser = Auth.getCurrentUser();
     vm.reportingFilters = FACILITY_FILTERS;
+    $scope.isSaving = {};
+    $scope.currentStatus = {};
+
+    vm.showToggleButton = function() {
+      return vm.currentUser.showStates && vm.currentUser.showZones && vm.currentUser.showLgas;
+    };
+
+    $scope.updateStatus = function(id) {
+      var appConfig = summaries.appConfig[id];
+      $scope.isSaving[id] = true;
+      AppConfig.put(appConfig.facility.email, {workingPhone: !$scope.currentStatus[id]})
+        .then(function () {
+          $scope.currentStatus[id] = !$scope.currentStatus[id];
+          $scope.isSaving[id] = false;
+        })
+        .catch(function (reason) {
+          $scope.isSaving[id] = false;
+          console.log(reason);
+        });
+    };
+
+    (function bindPhoneStatus() {
+      var rows = summaries.summaries;
+      for (var i = 0; i < rows.length; i++) {
+        $scope.currentStatus[rows[i].id] = angular.isUndefined(rows[i].workingPhone) ? true : rows[i].workingPhone;
+        $scope.isSaving[rows[i].id] = false;
+      }
+    })();
 
     function bindSummaries(summaries) {
       vm.summaries = summaries;
@@ -27,5 +55,5 @@ angular.module('lmisApp')
       .then(bindFacilityChart)
       .then(bindCSVData);
 
-    d.resolve(summaries);
+    d.resolve(summaries.summaries);
   });
