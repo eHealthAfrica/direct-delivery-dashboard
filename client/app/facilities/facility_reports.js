e7ae41065013974610ac8e8df6b8ebfbcbfb4b3b
Musa Musa <musakunte@gmail.com>
2015-08-03 11:14:41 +0100

    added more fields, removed redundant request to server

--- client/app/facilities/facility_reports.js
+++ client/app/facilities/facility_reports.js
@@ -4,6 +4,9 @@ angular.module('lmisApp')
   .factory('facilityReports', function($q, $window, stockCount, Facility) {
     function isNonReporting(lastReport, threshold) {
       // Time in days before a facility is classed as non reporting
+      if (lastReport === null) {
+        return true;
+      }
       threshold = threshold || 7;
       return lastReport > threshold;
     }
@@ -15,31 +18,37 @@ angular.module('lmisApp')
 
     function formatSummaries(resolvedPromises) {
       var summaries = resolvedPromises.summaries.summary;
-      var facilities = resolvedPromises.facilities;
+      var appConfig = resolvedPromises.summaries.appConfig;
 
-      return summaries.map(function(summary) {
-        var facilityDetail = facilities[summary.facilityUUID];
-        return {
-          zone: facilityDetail.zone,
-          lga: facilityDetail.lga,
-          facility: summary.facility,
-          isNonReporting: isNonReporting(summary.daysFromLastCountDate),
-          daysFromLastCountDate: summary.daysFromLastCountDate,
-          lastCountDate: formatDate(summary.mostRecentCountDate),
-          contact: {
-            name: facilityDetail.contact.name,
-            phone: facilityDetail.phone,
-            email: facilityDetail.email
-          }
-        };
-      });
+      return {
+        summaries: summaries.map(function (summary) {
+          var facilityDetail = appConfig[summary.facilityUUID].facility;
+          var contact = facilityDetail.contact || {};
+          return {
+            zone: facilityDetail.zone,
+            id: facilityDetail._id,
+            lga: facilityDetail.lga,
+            facility: summary.facility,
+            isNonReporting: isNonReporting(summary.daysFromLastCountDate),
+            daysFromLastCountDate: summary.daysFromLastCountDate,
+            lastCountDate: formatDate(summary.mostRecentCountDate),
+            createdDate: formatDate(summary.createdDate),
+            workingPhone: summary.workingPhone,
+            contact: {
+              name: contact.name || '',
+              phone: facilityDetail.phone,
+              email: facilityDetail.email
+            }
+          };
+        }),
+        appConfig: appConfig
+      }
     }
 
     return {
       load: function() {
         var facilityPromises = {
-          summaries: stockCount.stockCountSummaryByFacility(),
-          facilities: Facility.all()
+          summaries: stockCount.stockCountSummaryByFacility()
         };
 
         return $q.all(facilityPromises)
