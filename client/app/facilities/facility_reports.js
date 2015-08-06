ff5ac470849fe786e6b3d14dbd50adee0edf09f0
Musa Musa <musakunte@gmail.com>
2015-08-06 11:29:08 +0100

    created constant for report status, added reminder day to report object

--- client/app/facilities/facility_reports.js
+++ client/app/facilities/facility_reports.js
@@ -1,7 +1,50 @@
 'use strict';
 
 angular.module('lmisApp')
-  .factory('facilityReports', function($q, $window, stockCount, Facility) {
+  .factory('facilityReports', function($q, $window, stockCount) {
+    var FAULTY_PHONES = -1,
+        NON_REPORTING = 0,
+        ON_TIME_REPORT = 1,
+        DELAYING_REPORT = 2;
+
+    function getPhoneStatus(phoneStatusList) {
+      var statusList = !angular.isArray(phoneStatusList) ? [] : phoneStatusList;
+      var phoneObject = {};
+      if (statusList.length > 0) {
+        statusList =  statusList.sort(function (a, b) {
+          return new Date(b.modified).getTime() - new Date(a.modified).getTime();
+        });
+        phoneObject.fullList = statusList;
+        phoneObject.currentStatus = statusList[0].status;
+      } else {
+        phoneObject.fullList = statusList;
+        phoneObject.currentStatus = true;
+      }
+      return phoneObject;
+    }
+
+    function reportingStatus(lastReport, workingPhone) {
+      var status = 0;
+      if (!workingPhone) {
+        return FAULTY_PHONES
+      }
+      if (workingPhone && (lastReport > 7 && lastReport <= 14)) {
+        return DELAYING_REPORT;
+      }
+
+      if (workingPhone && (lastReport === null || lastReport > 14)) {
+        return NON_REPORTING;
+      }
+
+      if (workingPhone && lastReport <= 7) {
+        return ON_TIME_REPORT;
+      }
+
+
+      return status;
+
+    }
+
     function isNonReporting(lastReport, threshold) {
       // Time in days before a facility is classed as non reporting
       if (lastReport === null) {
@@ -9,22 +52,6 @@ angular.module('lmisApp')
       }
       threshold = threshold || 7;
       return lastReport > threshold;
-    }
-
-    function reportingStatus(lastReport) {
-      var state = '';
-      if (lastReport <= 7) {
-        state = 1;
-      }
-
-      if (lastReport > 7 && lastReport <= 14) {
-        state = 0;
-      }
-
-      if (lastReport > 14) {
-        state = -1;
-      }
-      return state;
     }
 
     function formatDate(date, dateTimeString) {
@@ -35,9 +62,11 @@ angular.module('lmisApp')
     function formatSummaries(resolvedPromises) {
       var summaries = resolvedPromises.summaries.summary;
       var appConfig = resolvedPromises.summaries.appConfig;
+      var groupedCounts = resolvedPromises.summaries.groupedStockCount;
 
       return {
         summaries: summaries.map(function (summary) {
+          var phoneStatus = getPhoneStatus(summary.workingPhone).currentStatus;
           var facilityDetail = appConfig[summary.facilityUUID].facility;
           var contact = facilityDetail.contact || {};
           return {
@@ -50,6 +79,9 @@ angular.module('lmisApp')
             lastCountDate: formatDate(summary.mostRecentCountDate),
             createdDate: formatDate(summary.createdDate),
             workingPhone: summary.workingPhone,
+            reminderDay: summary.reminderDay,
+            grouped: groupedCounts[summary.facilityUUID] || [],
+            reportingStatus: reportingStatus(summary.daysFromLastCountDate, phoneStatus),
             contact: {
               name: contact.name || '',
               phone: facilityDetail.phone,
@@ -62,6 +94,8 @@ angular.module('lmisApp')
     }
 
     return {
+      getPhoneStatus: getPhoneStatus,
+      getReportingStatus: reportingStatus,
       load: function() {
         var facilityPromises = {
           summaries: stockCount.stockCountSummaryByFacility()
