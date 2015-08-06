4e64546aa4d4801249e802f976aac50b17b9f11b
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-08-06 11:03:59 +0100

    change ccu breakdown logic

--- todo/design_docs/ccu_breakdown/by_latest_broken_date.js
+++ todo/design_docs/ccu_breakdown/by_latest_broken_date.js
@@ -1,7 +1,6 @@
 function(doc) {
 
 	function getLatestBroken(statusList) {
-		var recentDate;
 		var cceStatus;
 		var recentCCEStatus;
 		for (var k in statusList) {
@@ -9,28 +8,45 @@ function(doc) {
 			if (!recentCCEStatus) {
 				recentCCEStatus = cceStatus
 			}
-			if (cceStatus.created && cceStatus.status === 0 && (cceStatus.created < cceStatus.created)) {
+			if (cceStatus.created && (cceStatus.created < cceStatus.created)) {
 				recentCCEStatus = cceStatus;
 			}
 		}
 		return recentCCEStatus;
 	}
 
+	function pad(arg) {
+		arg = arg + ''; //cast to string
+		if(arg && arg.length === 1){
+			arg = ['0' , arg].join('');
+		}
+		return arg;
+	}
+
 
 	if (doc.facility && doc.created && doc.ccuProfile && doc.ccuStatus && doc.ccuStatus.length > 0) {
-		var latestBrokenStatus = getLatestBroken(doc.ccuStatus);
-		if (latestBrokenStatus) {
-			var date = new Date(latestBrokenStatus.created);
-			latestBrokenStatus.created = date;
+		var latestBrokenReport = getLatestBroken(doc.ccuStatus);
+		if (latestBrokenReport) {
+			var date = new Date(latestBrokenReport.created);
+			latestBrokenReport.created = date;
 			var cceBreakdownStatus = {
 				ccuProfile: {
 					modelId: doc.ccuProfile.dhis2_modelid,
 					id: doc.ccuProfile._id
 				},
-				status: latestBrokenStatus,
-				facility: doc.facility._id || doc.facility
+				status: latestBrokenReport.status,
+				facility: doc.facility._id || doc.facility,
+				created: new Date(latestBrokenReport.created)
 			};
-			emit(new Date(latestBrokenStatus.created), cceBreakdownStatus);
+
+			var date = new Date(latestBrokenReport.created);
+
+			var year = date.getFullYear();
+			var month = pad(date.getUTCMonth() + 1);
+			var dateDay = pad(date.getDate());
+
+			var key =  [ year, month, dateDay ].join('-');
+			emit(key, cceBreakdownStatus);
 		}
 
 	}
