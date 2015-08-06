e1340f085b38cbc3b8f72fd382d848a8ae97e6d8
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-08-06 11:31:28 +0100

    update cce breakdown view to include reported breakdown without detailed status information.

--- todo/design_docs/ccu_breakdown/by_latest_broken_date.js
+++ todo/design_docs/ccu_breakdown/by_latest_broken_date.js
@@ -23,31 +23,45 @@ function(doc) {
 		return arg;
 	}
 
-
+	var cceBreakdownStatus;
+	var createdDate;
 	if (doc.facility && doc.created && doc.ccuProfile && doc.ccuStatus && doc.ccuStatus.length > 0) {
 		var latestBrokenReport = getLatestBroken(doc.ccuStatus);
 		if (latestBrokenReport) {
-			var date = new Date(latestBrokenReport.created);
-			latestBrokenReport.created = date;
-			var cceBreakdownStatus = {
+			createdDate = new Date(latestBrokenReport.created);
+			latestBrokenReport.created = createdDate;
+			cceBreakdownStatus = {
 				ccuProfile: {
 					modelId: doc.ccuProfile.dhis2_modelid,
 					id: doc.ccuProfile._id
 				},
 				status: latestBrokenReport.status,
 				facility: doc.facility._id || doc.facility,
-				created: new Date(latestBrokenReport.created)
+				created: createdDate
 			};
-
-			var date = new Date(latestBrokenReport.created);
-
-			var year = date.getFullYear();
-			var month = pad(date.getUTCMonth() + 1);
-			var dateDay = pad(date.getDate());
-
-			var key =  [ year, month, dateDay ].join('-');
-			emit(key, cceBreakdownStatus);
 		}
-
+	}else if(doc.facility && doc.created && doc.ccuProfile){
+		//for breakdowns without ccuStatus list, maybe because it was sent and synced via SMS or
+		//Could also be Old or maybe even future cceBreakdown that does not have cceStatus property
+		//Workaround could be to include CCE faulty reason and created date as part of SMS sync for offline.
+		//SEE ISSUE #https://github.com/eHealthAfrica/move/issues/169
+		var FAULTY = 0;
+		createdDate = new Date(doc.created);
+		cceBreakdownStatus = {
+			ccuProfile: {
+				modelId: doc.ccuProfile.dhis2_modelid,
+				id: doc.ccuProfile._id
+			},
+			status: FAULTY,//assume it is faulty if status is unknown but reported.
+			facility: doc.facility._id || doc.facility,
+			created: createdDate
+		};
 	}
+
+	var year = createdDate.getFullYear();
+	var month = pad(createdDate.getUTCMonth() + 1);
+	var dateDay = pad(createdDate.getDate());
+
+	var key =  [ year, month, dateDay ].join('-');
+	emit(key, cceBreakdownStatus);
 }
