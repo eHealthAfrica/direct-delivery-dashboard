984e4b693fbb3c8cf7a86ba39defd7fb84a544b0
Musa Musa <musakunte@gmail.com>
2015-08-11 11:44:24 +0100

    Merge branch 'develop' into bug/landing-page-facilities-not-showing
    Conflicts:
    	client/app/main/main.controller.js

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -39,6 +39,7 @@ angular.module('lmisApp')
 		})
 		.controller('MainStockReport', function ($scope, facilityReports) {
 			$scope.working = true;
+      var reports = facilityReports.reportingConstants;
 			$scope.stockReports = {
 				noReports: [],
 				lateReports: [],
@@ -74,23 +75,22 @@ angular.module('lmisApp')
 			};
 			$scope.lateGridOption.data = [];
 
-			facilityReports.load()
-					.then(function (response) {
-						$scope.stockReports.total = response.length;
-						for (var i in response) {
-							if (response[i].isNonReporting) {
-								$scope.stockReports.noReports.push(response[i])
-							} else {
-								if (response[i].daysFromLastCountDate > 7) {
-									$scope.stockReports.lateReports.push(response[i])
-								}
-							}
-						}
-						$scope.lateGridOption.data = $scope.stockReports.lateReports;
-						$scope.gridOptions.data = $scope.stockReports.noReports;
-						$scope.working = false;
-					})
-					.catch(function (err) {
-						console.log(err);
-					});
+      facilityReports.load()
+        .then(function (response){
+          var stockCountSummaries = response.summaries;
+          $scope.stockReports.total = stockCountSummaries.length;
+          for(var i in stockCountSummaries){
+            if(stockCountSummaries[i].reportingStatus === reports.NON_REPORTING){
+              $scope.stockReports.noReports.push(stockCountSummaries[i])
+            }else if (stockCountSummaries[i].reportingStatus === reports.DELAYING_REPORT){
+              $scope.stockReports.lateReports.push(stockCountSummaries[i]);
+            }
+          }
+          $scope.lateGridOption.data = $scope.stockReports.lateReports;
+          $scope.gridOptions.data = $scope.stockReports.noReports;
+          $scope.working = false;
+        })
+        .catch(function(err){
+          console.log(err);
+        });
 		});
