8581fb8b825359b3e03a6981df63ddbd80e381c6
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-08-31 16:26:22 +0100

    add csv to json parser

--- src/app/planning/schedule/schedules.service.js
+++ src/app/planning/schedule/schedules.service.js
@@ -3,6 +3,23 @@ angular.module('planning')
 
 			var _this = this;
 
+			_this.getHeaders = function () {
+				return [
+					'Round Id', 'Facility Name', 'Facility Code', 'Delivery Date', 'Driver', 'Drop',
+					'Distance', 'Window'
+				];
+			};
+			
+			_this.headerIndex = {
+				roundId: 0,
+				facilityName: 1,
+				facilityCode: 2,
+				deliveryDate: 3,
+				driver: 4,
+				drop: 6,
+				distance: 7
+			};
+
 			_this.saveSchedules = function (schedules) {
 				return dbService.saveDocs(schedules);
 			};
@@ -47,15 +64,43 @@ angular.module('planning')
 					};
 				});
 
-				var headers = [
-					'Round Id', 'Facility Name', 'Facility Code', 'Delivery Date', 'Driver', 'Drop',
-					'Distance', 'Window'
-				];
-
 				return {
 					rows: rows,
-					header: headers
+					headers: _this.getHeaders()
 				};
+			};
+
+			_this.parseCSV = function (csvJSON) {
+				var result = {};
+				var headers = _this.getHeaders();
+				csvJSON.forEach(function(csvRow){
+					var row = {
+						roundId: csvRow[headers[0]],
+						facilityName: csvRow[headers[1]],
+						facilityCode: csvRow[headers[2]],
+						deliveryDate: csvRow[headers[3]],
+						driver: csvRow[headers[4]],
+						drop: csvRow[headers[5]],
+						distance: csvRow[headers[6]],
+						window: csvRow[headers[7]]
+					};
+					var rowHash = hashRow(row.roundId, row.facilityCode, row.driver, row.deliveryDate, row.drop);
+					result[rowHash] = row;
+				});
+				return result;
+			};
+
+			function hashRow(roundId, facilityCode, driver, date, drop){
+				return [ roundId, facilityCode, driver, date, drop ].join('-');
+			}
+
+			_this.mergeRows = function(dailyDeliveries, schedulesInfo){
+				var dailyDelivery;
+				for(var i in dailyDeliveries){
+					dailyDelivery = dailyDeliveries[i];
+					//TODO: complete implementation
+				}
+				return dailyDeliveries;
 			};
 
 		});
