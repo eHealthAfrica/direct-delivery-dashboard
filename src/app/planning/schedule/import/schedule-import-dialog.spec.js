2b60e48401021d9f097def7e8c2960b1081063ff
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-09-01 16:34:50 +0100

    add test for all import dialog controller functions and public variables.

--- src/app/planning/schedule/import/schedule-import-dialog.spec.js
+++ src/app/planning/schedule/import/schedule-import-dialog.spec.js
@@ -7,11 +7,13 @@ describe('ScheduleDataImportDialogCtrl', function () {
 
 	var ScheduleDataImportDialogCtrl;
 	var ScheduleRoundCtrl;
+	var scheduleService;
 	var $modal;
 	var $controller;
 	var deliveryRound;
 	var dailyDeliveries;
 	var log;
+	var csvResult;
 
 	var $modalInstance = {
 		result: {
@@ -33,23 +35,25 @@ describe('ScheduleDataImportDialogCtrl', function () {
 
 	var modalInstance;
 
-	beforeEach(inject(function (_$controller_, _$modal_, _scheduleService_, _planningService_, _$state_, _deliveryRoundMock_, _dailyDeliveriesMock_, _log_) {
+	beforeEach(inject(function (_$controller_, _$modal_, _scheduleService_, _planningService_,
+	                            _$state_, _deliveryRoundMock_, _dailyDeliveriesMock_, _log_,
+	                            _csvResultMock_) {
+
 		$controller = _$controller_;
 		$modal = _$modal_;
 		deliveryRound = _deliveryRoundMock_;
 		dailyDeliveries = _dailyDeliveriesMock_;
 		log = _log_;
+		scheduleService  = _scheduleService_;
+		csvResult = _csvResultMock_;
 
-		modalInstance = {                    // Create a mock object using spies
+		modalInstance = {
 			close: jasmine.createSpy('modalInstance.close'),
 			dismiss: jasmine.createSpy('modalInstance.dismiss'),
 			result: {
 				then: jasmine.createSpy('modalInstance.result.then')
 			}
 		};
-
-		spyOn($modal, 'open').and.returnValue($modalInstance);
-		spyOn($modalInstance, 'dismiss').and.callThrough();
 
 		ScheduleRoundCtrl = $controller('ScheduleRoundCtrl', {
 			deliveryRound: deliveryRound,
@@ -64,8 +68,15 @@ describe('ScheduleDataImportDialogCtrl', function () {
 		ScheduleDataImportDialogCtrl = $controller('ScheduleDataImportDialogCtrl', {
 			$modalInstance: modalInstance,
 			deliveryRound: deliveryRound,
-			dailyDeliveries: dailyDeliveries
+			dailyDeliveries: dailyDeliveries,
+			scheduleService: scheduleService
 		});
+
+		spyOn($modal, 'open').and.returnValue($modalInstance);
+		spyOn($modalInstance, 'dismiss').and.callThrough();
+		spyOn(scheduleService, 'parseCSV').and.callThrough();
+		spyOn(ScheduleDataImportDialogCtrl, 'getProcessedCSVRows').and.callThrough();
+		spyOn(scheduleService, 'applyChanges').and.callThrough();
 
 	}));
 
@@ -75,12 +86,120 @@ describe('ScheduleDataImportDialogCtrl', function () {
 		});
 	});
 
+	describe('roundCode', function(){
+		it('Should be defined', function(){
+			expect(ScheduleDataImportDialogCtrl.roundCode).toBeDefined();
+		});
+
+		it('Should equal delivery round passed dialog controller', function(){
+			expect(ScheduleDataImportDialogCtrl.roundCode).toEqual(deliveryRound._id);
+		});
+	});
+
+	describe('csv', function(){
+		it('Should have "result" property set to null by default', function(){
+			expect(ScheduleDataImportDialogCtrl.csv.result).toBeNull();
+		});
+
+		it('Should have "content" property set to null by default', function () {
+			expect(ScheduleDataImportDialogCtrl.csv.content).toBeNull();
+		});
+
+		it('Should have "header" set to true by default', function () {
+			expect(ScheduleDataImportDialogCtrl.csv.header).toBeTruthy();
+		});
+
+		it('Should have "separator" set to "," by default', function () {
+			expect(ScheduleDataImportDialogCtrl.csv.separator).toEqual(',');
+		});
+	});
+
+	describe('header', function (){
+    it('Should equal expected value', function(){
+	    var expected = scheduleService.getHeaders();
+	    expect(ScheduleDataImportDialogCtrl.headers).toEqual(expected);
+    });
+	});
+
 	describe('ScheduleDataImportDialogCtrl.close', function () {
 		it('should call $modalInstance.dismiss()', function () {
 			expect(modalInstance.dismiss).not.toHaveBeenCalled();
 			ScheduleDataImportDialogCtrl.close();
 			expect(modalInstance.dismiss).toHaveBeenCalled();
 		});
+	});
+
+	describe('getProcessedCSVRows()', function(){
+		it('Should NOT call scheduleService.parseCSV if csv is NOT Array', function(){
+			expect(scheduleService.parseCSV).not.toHaveBeenCalled();
+			ScheduleDataImportDialogCtrl.csv.result = null;
+			expect(ScheduleDataImportDialogCtrl.csv.result).toBeNull();
+			ScheduleDataImportDialogCtrl.getProcessedCSVRows();
+			expect(scheduleService.parseCSV).not.toHaveBeenCalledWith();
+		});
+
+		it('Should call scheduleService.parseCSV if it is an array', function(){
+			expect(scheduleService.parseCSV).not.toHaveBeenCalled();
+			ScheduleDataImportDialogCtrl.csv.result = [];
+			var isArray = angular.isArray(ScheduleDataImportDialogCtrl.csv.result);
+			expect(isArray).toBeTruthy();
+			ScheduleDataImportDialogCtrl.getProcessedCSVRows();
+			expect(scheduleService.parseCSV).not.toHaveBeenCalledWith();
+		});
+	});
+
+	describe('getSize()', function () {
+		it('Should return 0 if csv.result is null', function () {
+			expect(ScheduleDataImportDialogCtrl.csv.result).toBeNull();
+			var result = ScheduleDataImportDialogCtrl.getSize();
+			expect(result).toBe(0);
+		});
+
+		it('Should return expected size if csv.result is not empty array', function(){
+			ScheduleDataImportDialogCtrl.csv.result = csvResult;
+			var result = ScheduleDataImportDialogCtrl.getSize();
+			expect(result).toBeGreaterThan(0);
+		});
+	});
+
+	describe('isValid', function () {
+		it('should return TRUE if row matches given round code', function () {
+			var csvRow = csvResult[0];
+			var expectedRoundCode = csvRow[ScheduleDataImportDialogCtrl.headers.roundCode.text];
+			expect(ScheduleDataImportDialogCtrl.roundCode).toEqual(expectedRoundCode);
+			expect(ScheduleDataImportDialogCtrl.isValid(csvRow)).toBeTruthy();
+		});
+
+		it('Should return FALSE if row and delivery round code differs', function () {
+			var csvRow = csvResult[0];
+			csvRow[ScheduleDataImportDialogCtrl.headers.roundCode.text] = 'KKK 245 908';
+			var expectedRoundCode = csvRow[ScheduleDataImportDialogCtrl.headers.roundCode.text];
+			expect(ScheduleDataImportDialogCtrl.roundCode).not.toEqual(expectedRoundCode);
+			expect(ScheduleDataImportDialogCtrl.isValid(csvRow)).toBeFalsy();
+		});
+	});
+
+	describe('applyImport', function () {
+		it('Should call getProcessedCSVRows()', function () {
+			expect(ScheduleDataImportDialogCtrl.getProcessedCSVRows).not.toHaveBeenCalled();
+			ScheduleDataImportDialogCtrl.applyImport();
+			expect(ScheduleDataImportDialogCtrl.getProcessedCSVRows).toHaveBeenCalled();
+		});
+
+		it('Should call scheduleService.applyChanges(param1, param2)', function () {
+			expect(scheduleService.applyChanges).not.toHaveBeenCalled();
+			ScheduleDataImportDialogCtrl.applyImport();
+			expect(scheduleService.applyChanges).toHaveBeenCalled();
+		});
+
+		it('Should call $modalInstance.close with expected params', function () {
+			expect(modalInstance.close).not.toHaveBeenCalled();
+			ScheduleDataImportDialogCtrl.applyImport();
+			var scheduleRowsHash = ScheduleDataImportDialogCtrl.getProcessedCSVRows();
+			var expectedParam = scheduleService.applyChanges(dailyDeliveries, scheduleRowsHash);
+			expect(modalInstance.close).toHaveBeenCalledWith(expectedParam);
+		});
+
 	});
 
 });
