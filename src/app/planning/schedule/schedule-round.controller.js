2f64f9a1d0b37b685abb09c7849091aa3a196165
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-09-05 14:47:55 +0100

    implement controller function required by inline schedule editing

--- src/app/planning/schedule/schedule-round.controller.js
+++ src/app/planning/schedule/schedule-round.controller.js
@@ -1,9 +1,18 @@
 'use strict';
 
 angular.module('planning')
-		.controller('ScheduleRoundCtrl', function (deliveryRound, $state, dailyDeliveries, scheduleService, planningService, log, $modal) {
+		.controller('ScheduleRoundCtrl', function ($scope, deliveryRound, $state, dailyDeliveries, scheduleService, planningService, log, $modal, utility) {
 
 			var vm = this;
+
+			//TODO: set to drivers list pulled from database
+			vm.drivers = [
+				{value: 'bashir@example.com', text: 'bashir@example.com'},
+				{value: 'abdullahi@example.com', text: 'abdullahi@example.com'},
+				{value: 'khalil@example.com', text: 'khalil@example.com'},
+				{value: 'umar@example.com', text: 'umar@example.com'}
+			];
+
 
 			vm.deliveryRound = deliveryRound;
 			vm.deliveriesHash = {};
@@ -13,7 +22,7 @@ angular.module('planning')
 						vm.deliveriesHash[hashId] = row;
 					});
 
-			function updateDeliveries (deliveries) {
+			function updateDeliveries(deliveries) {
 				vm.dailyDeliveries = deliveries;
 				vm.facilityDeliveries = scheduleService.flatten(vm.dailyDeliveries);
 			}
@@ -44,13 +53,47 @@ angular.module('planning')
 						.catch(scheduleService.onSaveError);
 			};
 
-			vm.isUpdated = function(row){
+			vm.getHash = function (row) {
+				return scheduleService.hashRow(row.deliveryRoundID, row.facility.id, row._id);
+			};
+
+			vm.isUpdated = function (row) {
 				var hashId = scheduleService.hashRow(row.deliveryRoundID, row.facility.id, row._id);
-        var before  = vm.deliveriesHash[hashId];
-				if(before){
+				var before = vm.deliveriesHash[hashId];
+				if (before) {
 					return !angular.equals(before, row);
 				}
 				return false;
+			};
+
+			vm.getDate = function(date){
+				if(utility.isValidDate(date)){
+					return utility.formatDate(date);
+				}
+				return ''
+			};
+
+			vm.saveRow = function (data, row) {
+				var updatedRow = {
+					deliveryDate: new Date(row.date).toJSON(),
+					distance: data.distance,
+					driver: data.driverID,
+					drop: data.drop,
+					facilityCode: row.facility.id,
+					facilityName: row.facility.name,
+					id: row._id,
+					roundId: vm.deliveryRound._id,
+					window: data.window
+				};
+				var hashId = scheduleService.hashRow(vm.deliveryRound._id, row.facility.id, row._id);
+				var hashUpdate = {};
+				hashUpdate[hashId] = updatedRow;
+				updateDeliveries(scheduleService.applyChanges(dailyDeliveries, hashUpdate));
+			};
+
+			vm.cancel = function (index) {
+				//TODO: imeplement cancel workflow if necessary
+				console.info('Clicked on cancel', index);
 			};
 
 			vm.openImportDialog = function () {
