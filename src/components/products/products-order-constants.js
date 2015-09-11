dd0dba92a2053ed00d1a6f394aae66f54c2defb4
Musa Musa <musakunte@gmail.com>
2015-09-11 09:50:17 +0100

    added product order constant

---
+++ src/components/products/products-order-constants.js
@@ -0,0 +1,22 @@
+angular.module('products')
+  .constant('PRODUCT_ORDER', {
+    'bcg': 1,
+    'mv': 2,
+    'yf': 3,
+    'opv': 4,
+    'ipv': 5,
+    'tt': 6,
+    'penta': 7,
+    'hbv': 8,
+    'yf dil': 9,
+    'mv dil': 10,
+    'bcg dil': 11,
+    '0.05ml': 12,
+    '0.5ml': 13,
+    'rc 2ml': 14,
+    'rc 5ml': 15,
+    'safety boxes': 16,
+    'droppers': 17,
+    'cards': 18,
+  });
+
