18503345850bc86192b0c0d700a1a60263ff749b
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-09-11 16:39:08 +0100

    collate lga list by facility lga list. start implementing function to save updated rows

--- src/app/planning/allocation/delivery-allocation.service.js
+++ src/app/planning/allocation/delivery-allocation.service.js
@@ -3,12 +3,57 @@ angular.module('planning')
 
 			var _this = this;
 
-			_this.getAllocationBy = function (roundId) {
+			_this.update = function(docId, facilityId, packedProductHash){
+				return dbService.get(docId)
+						.then(function (doc) {
+							var facRnd = doc;
+							if(angular.isArray(doc.facilityRounds)){
+								facRnd = null;
+								for(var i in doc.facilityRounds){
+									var temp = doc.facilityRounds[i];
+									if(temp.facility && temp.facility.id === facilityId){
+										facRnd = temp;
+										break;
+									}
+								}
+							}
+							if(!facRnd.packedProduct){
+								facRnd.packedProduct = [];
+							}
+							var ppHash = utility.hashBy(facRnd.packedProduct, 'productID');
+							var expectedQty;
+							var updatedPackedProducts = [];
+							for (var k in packedProductHash) {
+								expectedQty = packedProductHash[k];
+								var packedProduct = ppHash[k];
+								if(packedProduct){
+									packedProduct.expectedQty = expectedQty;
+									updatedPackedProducts.push(packedProduct);
+								}
+							}
+
+							//else{
+							//	facRnd.packedProduct = facRnd.packedProduct
+							//			.map(function(pp) {
+							//				var expectedQty = packedProductHash[pp.productID];
+							//				if(angular.isNumber(expectedQty)){
+							//					pp.expectedQty = expectedQty;
+							//				}
+							//				return pp;
+							//			});
+							//}
+							//TODO: recalculate packing list quantity
+
+						});
+			};
+
+			_this.getAllocationBy = function (roundId, lga) {
 				var view = 'dashboard-delivery-rounds/facility-allocation-by-round';
 				var params = {
 					key: roundId
 				};
 				var uniqueProductList = [];
+				var lgaList = [];
 				return dbService.getView(view, params)
 						.then(function (res) {
 							if (res.rows.length === 0) {
@@ -25,12 +70,18 @@ angular.module('planning')
 										}
 									});
 								}
+								if(row.facility && row.facility.lga && lgaList.indexOf(row.facility.lga) === -1){
+									lgaList.push(row.facility.lga);
+								}
 								row.packedProduct = packedProductHash;
 								return row;
+							}).filter(function(row){
+								return row.facility && row.facility.lga === lga;
 							});
 							return {
 								rows: resultSet,
-								productList: uniqueProductList
+								productList: uniqueProductList,
+								lgaList: lgaList.sort()
 							}
 						});
 			};
