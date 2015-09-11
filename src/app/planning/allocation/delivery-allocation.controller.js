02abc1fe8e4149d42ff2003bcb72200d712b2708
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-09-11 16:36:48 +0100

    update table by selected lga

--- src/app/planning/allocation/delivery-allocation.controller.js
+++ src/app/planning/allocation/delivery-allocation.controller.js
@@ -1,25 +1,38 @@
 angular.module('planning')
-		.controller('DeliveryAllocationCtrl', function (deliveryRound, facilityAllocationInfo) {
+		.controller('DeliveryAllocationCtrl', function (deliveryRound, facilityAllocationInfo, deliveryAllocationService, log) {
 
 			var vm = this;
 			vm.views = {
 				packedProduct: 'Packed Product',
 				productPresentation: 'Product Presentation'
 			};
+			vm.selectedLGA = '';
 			vm.productPresentation = {};
 
 			vm.deliveryRound = deliveryRound;
 			vm.selectedView = vm.views.packedProduct;
-			vm.facilityRounds = facilityAllocationInfo.rows;
-			vm.productList = facilityAllocationInfo.productList;
-
+			vm.facAllocInfo = facilityAllocationInfo;
 
 			vm.switchView = function (view) {
 				vm.selectedView = view;
 			};
 
-			vm.saveRow = function ($data, $index) {
+			vm.updateList = function() {
+				deliveryAllocationService.getAllocationBy(vm.deliveryRound._id, vm.selectedLGA)
+						.then(function(result) {
+							vm.facAllocInfo = result;
+						})
+						.catch(vm.handleError);
+			};
 
+			vm.handleError = function(err){
+				log.error('getAllocationError', err);
+			};
+
+			vm.saveRow = function ($data, facRnd) {
+				//TODO: complete implementation
+				console.log(facRnd._id, facRnd.facility.id);
+				console.log('Data : ', $data, 'Facility Round : ', facRnd);
 			};
 
 
