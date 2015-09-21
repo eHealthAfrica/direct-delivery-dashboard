aa00e6cc42b04f01c79937c48515dfab1c030720
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-09-21 15:19:53 +0100

    update and save row changes

--- src/app/planning/return-route/return-route.controller.js
+++ src/app/planning/return-route/return-route.controller.js
@@ -1,10 +1,38 @@
 'use strict';
 
 angular.module('planning')
-		.controller('ReturnRouteCtrl', function (deliveryRound) {
+		.controller('ReturnRouteCtrl', function (deliveryRound, packingStores, deliveryReturnRoutes, utility, returnRouteService, log) {
 
 			var vm = this;
-			vm.deliveryRound = deliveryRound;
 
+			vm.query = '';
+			vm.deliveryRound = deliveryRound;
+			vm.deliveryReturnRoutes = deliveryReturnRoutes;
+			vm.packingStores = packingStores;
+
+			vm.getDocBy = function(driverId, deliveryDate) {
+				return utility.takeFirst(vm.deliveryReturnRoutes
+						.filter(function (row) {
+							return (row.driverID === driverId && row.deliveryDate === deliveryDate);
+						}));
+			};
+
+			vm.saveRow = function ($data, driverId, deliveryDate) {
+				var doc = vm.getDocBy(driverId, deliveryDate);
+				if(doc){
+					for(var k in $data){
+						var value = $data[k];
+						if(value){
+							doc[k] = value;
+						}
+					}
+					return returnRouteService.save(doc)
+							.then(function (res){
+								doc = res;
+								log.success('returnRouteSaved');
+							})
+							.catch(returnRouteService.onSaveError);
+				}
+			};
 
 		});
