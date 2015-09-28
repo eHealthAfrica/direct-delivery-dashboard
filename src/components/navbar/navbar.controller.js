25668d469350d2b0e2f4b07b1746bb5b8ce204df
Tom Vincent <git@tlvince.com>
2015-09-28 17:49:10 +0100

    fix: redirect to login on logout
    If the user was in a state requiring authorisation, a state change error would
    be thrown as soon as they logged out. Fix it by always redirecting to the login
    page.
    
    Add a error handler while we're here.

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -1,14 +1,13 @@
 'use strict';
 
 angular.module('navbar')
-  .controller('NavbarCtrl', function($state, config, navbarService, ehaCouchDbAuthService) {
+  .controller('NavbarCtrl', function($state, log, config, navbarService, ehaCouchDbAuthService) {
     this.name = config.name;
     this.items = navbarService.get();
 
     this.logout = function() {
       ehaCouchDbAuthService.signOut()
-        .then(function() {
-          $state.reload();
-        });
+        .then($state.go.bind($state, 'login'))
+        .catch(log.error('logoutFailed'));
     };
   });
