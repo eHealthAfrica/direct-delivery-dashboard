5eac8678f4702bdd952ad368339b1fa0f1e4dcbd
Tom Vincent <git@tlvince.com>
2015-09-28 17:42:43 +0100

    fix: always authorise admins

--- src/app/planning/planning.states.js
+++ src/app/planning/planning.states.js
@@ -9,7 +9,15 @@ angular.module('planning')
 				templateUrl: 'app/planning/planning.html',
         resolve: {
           authentication: ehaCouchDbAuthServiceProvider.requireAuthenticatedUser,
-          authorization: ehaCouchDbAuthServiceProvider.requireDirectDeliveryDashboardGisUser
+          authorization: function(authService) {
+            // XXX: we'd prefer to use ehaCouchDbAuthServiceProvider's
+            //      require<Role>User convenience method here, but that doesn't
+            //      auto-authorise admins. See:
+            //      https://github.com/eHealthAfrica/angular-eha.couchdb-auth/issues/28
+            return authService.requireRoles([
+              'direct_delivery_dashboard_gis'
+            ]);
+          }
         },
         data: {
           roles: [
--- src/components/auth/auth.service.js
+++ src/components/auth/auth.service.js
@@ -1,8 +1,14 @@
 'use strict';
 
 angular.module('auth')
-  .service('authService', function($q, ehaCouchDbAuthService) {
+  .service('authService', function($q, config, ehaCouchDbAuthService) {
     this.requireRoles = function(roles) {
+      roles = roles || [];
+      // Always authorise admins
+      // TODO: remove depending on
+      //       https://github.com/eHealthAfrica/angular-eha.couchdb-auth/issues/28
+      roles = roles.concat(config.admin.roles);
+
       function hasRoles(user) {
         return user.hasRole(roles) ? true : $q.reject('unauthorized');
       }
