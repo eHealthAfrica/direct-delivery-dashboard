9e5687e8bb706dc0963fb622f94166efd3835335
Tom Vincent <git@tlvince.com>
2015-10-06 15:29:25 +0100

    chore: restore coverage report

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,5 @@
 'use strict';
+/*eslint-env node */
 
 var path = require('path');
 var conf = require('./gulp/conf');
@@ -42,17 +43,26 @@ module.exports = function(config) {
       moduleName: 'directDeliveryDashboard'
     },
 
-    browsers : ['PhantomJS'],
+    browsers: ['PhantomJS'],
 
-    plugins : [
-      'karma-phantomjs-launcher',
-      'karma-angular-filesort',
-      'karma-jasmine',
-      'karma-ng-html2js-preprocessor'
+    reporters: [
+      'progress',
+      'coverage'
     ],
 
     preprocessors: {
-      'src/**/*.html': ['ng-html2js']
+      'src/**/*.html': ['ng-html2js'],
+      'src/app/**/!(*.spec|*.mock).js': ['coverage']
+    },
+
+    coverageReporter: {
+      dir: 'coverage',
+      reporters: [
+        {
+          type: 'lcov',
+          subdir: 'lcov'
+        }
+      ]
     }
   };
 
@@ -60,7 +70,7 @@ module.exports = function(config) {
   // If you ever plan to use Chrome and Travis, you can keep it
   // If not, you can safely remove it
   // https://github.com/karma-runner/karma/issues/1144#issuecomment-53633076
-  if(configuration.browsers[0] === 'Chrome' && process.env.TRAVIS) {
+  if (configuration.browsers[0] === 'Chrome' && process.env.TRAVIS) {
     configuration.customLaunchers = {
       'chrome-travis-ci': {
         base: 'Chrome',
