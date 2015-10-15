924028a9b7075ed31f81c9daaf9f1d091ae6263a
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-10-15 16:40:37 +0100

    KPI tab state and controller

---
+++ src/app/planning/kpi/kpi.controller.js
@@ -0,0 +1,23 @@
+'use strict'
+
+angular.module('planning')
+		.controller('KPIController', function (deliveryRound, kpiTemplates,  kpiInfo) {
+			var vm = this
+
+			vm.deliveryRound = deliveryRound
+			vm.kpiTemplates = kpiTemplates
+			vm.antigens = kpiInfo.antigens
+			vm.facilityKPIList = kpiInfo.kpiList
+
+			vm.getDriver = function (driver) {
+				if(angular.isString(driver)){
+					return driver.split('@')[0]
+				}
+				return ''
+			}
+
+			vm.saveRow = function () {
+				//TODO: complete implementation
+			}
+
+		})
