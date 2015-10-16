7f1eca356b47ad3292d46e3e5a50cfc239bafaca
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-10-16 11:53:51 +0100

    fix standardjs formatting

--- src/app/planning/kpi/kpi.controller.js
+++ src/app/planning/kpi/kpi.controller.js
@@ -1,23 +1,20 @@
 'use strict'
 
-angular.module('planning')
-		.controller('KPIController', function (deliveryRound, kpiTemplates,  kpiInfo) {
-			var vm = this
+angular.module('planning').controller('KPIController', function (deliveryRound, kpiTemplates, kpiInfo) {
+  var vm = this
+  vm.deliveryRound = deliveryRound
+  vm.kpiTemplates = kpiTemplates
+  vm.antigens = kpiInfo.antigens
+  vm.facilityKPIList = kpiInfo.kpiList
 
-			vm.deliveryRound = deliveryRound
-			vm.kpiTemplates = kpiTemplates
-			vm.antigens = kpiInfo.antigens
-			vm.facilityKPIList = kpiInfo.kpiList
+  vm.getDriver = function (driver) {
+    if (angular.isString(driver)) {
+      return driver.split('@')[0]
+    }
+    return ''
+  }
 
-			vm.getDriver = function (driver) {
-				if(angular.isString(driver)){
-					return driver.split('@')[0]
-				}
-				return ''
-			}
-
-			vm.saveRow = function () {
-				//TODO: complete implementation
-			}
-
-		})
+  vm.saveRow = function () {
+    // TODO: complete implementation
+  }
+	})
