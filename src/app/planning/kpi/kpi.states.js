e9647c9f9f910ad049c235ce861b30225009cbf4
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-10-16 12:40:29 +0100

    kpi-info state standardjs formatting

--- src/app/planning/kpi/kpi.states.js
+++ src/app/planning/kpi/kpi.states.js
@@ -1,37 +1,36 @@
 'use strict'
 
-angular.module('planning')
-		.config(function ($stateProvider) {
-			$stateProvider.state('planning.kpi', {
-				url: '/kpi/:roundId',
-				templateUrl: 'app/planning/kpi/index.html',
-				controller: 'KPIController',
-				controllerAs: 'kpiCtrl',
-				resolve: {
-					deliveryRound: function (log, planningService, $stateParams) {
-						function handleError (err) {
-							log.error('deliveryRoundNotFound', err)
-							throw err // block $state transition
-						}
-						return planningService.getByRoundId($stateParams.roundId)
-								.catch(handleError)
-					},
-					kpiTemplates:  function (kpiService) {
-						return kpiService.getAllTemplates()
-								.catch(function () {
+angular.module('planning').config(function ($stateProvider) {
+  $stateProvider.state('planning.kpi', {
+    url: '/kpi/:roundId',
+    templateUrl: 'app/planning/kpi/index.html',
+    controller: 'KPIController',
+    controllerAs: 'kpiCtrl',
+    resolve: {
+      deliveryRound: function (log, planningService, $stateParams) {
+        function handleError (err) {
+          log.error('deliveryRoundNotFound', err)
+          throw err // block $state transition
+        }
 
-								});
-					},
-					kpiInfo: function (log, kpiService, $stateParams) {
-						return kpiService.getByRoundId($stateParams.roundId)
-								.catch(function (err) {
-									log.error('getDeliveryRoundKPIListErr', err)
-									return {
-										antigens: [],
-										kpiList: []
-									}
-								})
-					}
-				}
-			})
-		})
+        return planningService.getByRoundId($stateParams.roundId).catch(handleError)
+      },
+      kpiTemplates: function (kpiService) {
+        return kpiService.getAllTemplates().catch(function () {
+          // TODO: add error message
+          return []
+        })
+      },
+      kpiInfo: function (log, kpiService, $stateParams) {
+        return kpiService.getByRoundId($stateParams.roundId)
+          .catch(function (err) {
+            log.error('getDeliveryRoundKPIListErr', err)
+            return {
+              antigens: [],
+              kpiList: []
+            }
+          })
+      }
+    }
+  })
+})
