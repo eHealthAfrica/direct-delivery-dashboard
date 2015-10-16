b10abc4325c10f1b413f271f7d03d0c296da3ba2
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-10-16 16:15:15 +0100

    add save function and on save error handler

--- src/app/planning/kpi/kpi.controller.js
+++ src/app/planning/kpi/kpi.controller.js
@@ -1,6 +1,6 @@
 'use strict'
 
-angular.module('planning').controller('KPIController', function (deliveryRound, kpiTemplates, kpiInfo) {
+angular.module('planning').controller('KPIController', function (deliveryRound, kpiTemplates, kpiInfo, kpiService, log) {
   var vm = this
   vm.deliveryRound = deliveryRound
   vm.kpiTemplates = kpiTemplates
@@ -14,7 +14,31 @@ angular.module('planning').controller('KPIController', function (deliveryRound, 
     return ''
   }
 
-  vm.saveRow = function () {
-    // TODO: complete implementation
+  vm.onSaveError = function (err) {
+    if (err.status === 401) {
+      return log.error('unauthorizedAccess', err)
+    }
+    if (err.status === 409) {
+      return log.error('updateConflict', err)
+    }
+    return log.error('saveReturnRouteErr', err)
   }
-	})
+
+  vm.saveRow = function ($data, kpiList, $index) {
+    var facKPI = kpiList[$index]
+    var tempDoc = angular.copy(facKPI)
+    tempDoc.antigensKPI = tempDoc.antigensKPI
+      .map(function (antigenKPI) {
+        var update = $data[antigenKPI.productID]
+        if (antigenKPI && angular.isNumber(update) && angular.isNumber(antigenKPI.noImmunized)) {
+          antigenKPI.noImmunized = update
+        }
+        return antigenKPI
+      })
+    return kpiService.save(tempDoc)
+      .then(function () {
+        return log.success('saveKPISuccess')
+      })
+      .catch(vm.onSaveError)
+  }
+})
