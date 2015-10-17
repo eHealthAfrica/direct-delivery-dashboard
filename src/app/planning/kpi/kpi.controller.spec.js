52c8d218d67b7b4263c8e2f15668735711333bdc
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-10-17 14:13:10 +0100

    standard js formatting

--- src/app/planning/kpi/kpi.controller.spec.js
+++ src/app/planning/kpi/kpi.controller.spec.js
@@ -2,113 +2,109 @@
 /* global describe, beforeEach, it, inject, expect, module, spyOn */
 
 describe('KPIController', function () {
-	beforeEach(module('planning', 'deliveryMock', 'utility', 'log', 'kpiMock'))
+  beforeEach(module('planning', 'deliveryMock', 'utility', 'log', 'kpiMock'))
 
-	var $controller
-	var log
-	var KPIController
-	var kpiTemplates
-	var kpiInfo
-	var kpiService
-	var deliveryRound
+  var $controller
+  var log
+  var KPIController
+  var kpiTemplates
+  var kpiInfo
+  var kpiService
+  var deliveryRound
 
+  beforeEach(inject(function (_$controller_, _deliveryRoundMock_, _log_,
+    _kpiTemplatesMock_, _kpiInfoMock_, _kpiService_) {
+    $controller = _$controller_
+    log = _log_
+    kpiService = _kpiService_
+    // copy to avoid modifying same object in each test run
+    kpiInfo = angular.copy(_kpiInfoMock_)
+    deliveryRound = angular.copy(_deliveryRoundMock_)
+    kpiTemplates = angular.copy(_kpiTemplatesMock_)
 
-	beforeEach(inject(function (_$controller_, _deliveryRoundMock_, _log_,
-	                            _kpiTemplatesMock_, _kpiInfoMock_, _kpiService_) {
+    KPIController = $controller('KPIController', {
+      deliveryRound: deliveryRound,
+      kpiTemplates: kpiTemplates,
+      kpiInfo: kpiInfo,
+      kpiService: kpiService,
+      log: log
+    })
 
-		$controller = _$controller_
-		log = _log_
-		kpiService = _kpiService_
-		//copy to avoid modifying same object in each test run
-		kpiInfo =  angular.copy(_kpiInfoMock_)
-		deliveryRound = angular.copy(_deliveryRoundMock_)
-		kpiTemplates = angular.copy(_kpiTemplatesMock_)
+    spyOn(log, 'error').and.callThrough()
+    spyOn(kpiService, 'save').and.callThrough()
+  }))
 
-		KPIController = $controller('KPIController', {
-			deliveryRound: deliveryRound,
-			kpiTemplates: kpiTemplates,
-			kpiInfo: kpiInfo,
-			kpiService: kpiService,
-			log: log
-		})
+  describe('KPIController', function () {
+    it('Should be defined or instantiated', function () {
+      expect(KPIController).toBeDefined()
+    })
+  })
 
-		spyOn(log, 'error').and.callThrough()
-		spyOn(kpiService, 'save').and.callThrough()
+  it('Should set KPIController.deliveryRound to expected object', function () {
+    expect(KPIController.deliveryRound).toBe(deliveryRound)
+  })
 
-	}))
+  it('Should set KPIController.kpiTemplate to expected value', function () {
+    expect(KPIController.kpiTemplates).toBe(kpiTemplates)
+  })
 
-	describe('KPIController', function () {
-		it('Should be defined or instantiated', function () {
-			expect(KPIController).toBeDefined()
-		})
-	})
+  it('Should set KPIController.antigens to expected value', function () {
+    expect(KPIController.antigens).toBe(kpiInfo.antigens)
+  })
 
-	it('Should set KPIController.deliveryRound to expected object', function () {
-		expect(KPIController.deliveryRound).toBe(deliveryRound)
-	})
+  it('Should set KPIController.facilityKPIList to expected value', function () {
+    expect(KPIController.facilityKPIList).toBe(kpiInfo.kpiList)
+  })
 
-	it('Should set KPIController.kpiTemplate to expected value', function () {
-		expect(KPIController.kpiTemplates).toBe(kpiTemplates)
-	})
+  describe('getDriver', function () {
+    it('Should return expected formatted value', function () {
+      var driverId = 'bashir@example.com'
+      var result = KPIController.getDriver(driverId)
+      var expected = 'bashir'
+      expect(result).toBe(expected)
+    })
 
-	it('Should set KPIController.antigens to expected value', function () {
-		expect(KPIController.antigens).toBe(kpiInfo.antigens)
-	})
+    it('Should return EMPTY string if given non-string', function () {
+      var driverId = {}
+      var result = KPIController.getDriver(driverId)
+      var expected = ''
+      expect(result).toBe(expected)
+    })
+  })
 
-	it('Should set KPIController.facilityKPIList to expected value', function () {
-		expect(KPIController.facilityKPIList).toBe(kpiInfo.kpiList)
-	})
+  describe('onSaveError', function () {
+    it('Should  call log.error() with expected parameter if status is 401', function () {
+      expect(log.error).not.toHaveBeenCalled()
+      var err = { status: 401 }
+      KPIController.onSaveError(err)
+      expect(log.error).toHaveBeenCalledWith('unauthorizedAccess', err)
+    })
 
-	describe('getDriver', function () {
-		it('Should return expected formatted value', function () {
-			var driverId = 'bashir@example.com'
-			var result  = KPIController.getDriver(driverId)
-			var expected = 'bashir'
-			expect(result).toBe(expected)
-		})
+    it('Should  call log.error() with expected parameter if status is 409', function () {
+      expect(log.error).not.toHaveBeenCalled()
+      var err = { status: 409 }
+      KPIController.onSaveError(err)
+      expect(log.error).toHaveBeenCalledWith('updateConflict', err)
+    })
 
-		it('Should return EMPTY string if given non-string', function () {
-			var driverId = {}
-			var result = KPIController.getDriver(driverId)
-			var expected = ''
-			expect(result).toBe(expected)
-		})
-	})
+    it('Should call log.error() with expected parameter if status is unknown', function () {
+      expect(log.error).not.toHaveBeenCalled()
+      var err = 'Unknown Error'
+      KPIController.onSaveError(err)
+      expect(log.error).toHaveBeenCalledWith('saveKPIError', err)
+    })
+  })
 
-	describe('onSaveError', function () {
-		it('Should  call log.error() with expected parameter if status is 401', function () {
-			expect(log.error).not.toHaveBeenCalled()
-			var err = { status: 401 };
-			KPIController.onSaveError(err)
-			expect(log.error).toHaveBeenCalledWith('unauthorizedAccess', err)
-		})
-
-		it('Should  call log.error() with expected parameter if status is 409', function () {
-			expect(log.error).not.toHaveBeenCalled()
-			var err = { status: 409 };
-			KPIController.onSaveError(err)
-			expect(log.error).toHaveBeenCalledWith('updateConflict', err)
-		})
-
-		it('Should call log.error() with expected parameter if status is unknown', function () {
-			expect(log.error).not.toHaveBeenCalled()
-			var err = 'Unknown Error';
-			KPIController.onSaveError(err)
-			expect(log.error).toHaveBeenCalledWith('saveKPIError', err)
-		})
-	})
-
-	describe('saveRow', function () {
-		it('Should call kpiService.save(tempDoc)', function () {
-			expect(kpiService.save).not.toHaveBeenCalled()
-			var $data = {
-				notes: 'test notes',
-				outreachSessions: "2"
-			}
-			var $index = 0;
-			KPIController.saveRow($data, KPIController.facilityKPIList, $index)
-			expect(kpiService.save).toHaveBeenCalledWith()
-		})
-	})
-
+  describe('saveRow', function () {
+    it('Should call kpiService.save(tempDoc)', function () {
+      expect(kpiService.save).not.toHaveBeenCalled()
+      var $data = {
+        notes: 'test notes',
+        outreachSessions: '2'
+      }
+      var $index = 0
+      KPIController.saveRow($data, KPIController.facilityKPIList, $index)
+      expect(kpiService.save).toHaveBeenCalledWith()
+    })
+  })
 })
--- src/app/planning/kpi/kpi.mock.js
+++ src/app/planning/kpi/kpi.mock.js
@@ -1,189 +1,189 @@
 'use strict'
 
 angular.module('kpiMock', [])
-	.constant('kpiTemplatesMock', [
-		{
-			"_id": "kpi-template-1",
-			"name": "Number Immunized",
-			"outreachSessions": "",
-			"notes": "",
-			"doc_type": "kpi-template",
-			"date": "",
-			"originRow": "",
-			"deliveryRoundID": "",
-			"driverID": "",
-			"version": "2.0.0",
-			"worksheetId": "ojuogcm",
-			"spreadsheetId": "",
-			"importedAt": "",
-			"antigensKPI": [
-				{
-					"productID": "BCG",
-					"noImmunized": 0
-				},
-				{
-					"productID": "MV",
-					"noImmunized": 0
-				},
-				{
-					"productID": "OPV",
-					"noImmunized": 0
-				},
-				{
-					"productID": "IPV",
-					"noImmunized": 0
-				},
-				{
-					"productID": "TT",
-					"noImmunized": 0
-				},
-				{
-					"productID": "Penta",
-					"noImmunized": 0
-				},
-				{
-					"productID": "HBV",
-					"noImmunized": 0
-				},
-				{
-					"productID": "YF",
-					"noImmunized": 0
-				}
-			],
-			"facility": {
-				"zone": "",
-				"lga": "",
-				"ward": "",
-				"name": "",
-				"id": ""
-			}
-		}
-	])
-	.constant('kpiInfoMock', {
-		"antigens": [
-			"BCG",
-			"MV",
-			"OPV",
-			"IPV",
-			"TT",
-			"Penta",
-			"HBV",
-			"YF"
-		],
-		kpiList: [
-			{
-				"_id": "0f56b4f46d2474b990a43721090004cb",
-				"_rev": "30-4d48c732226ac3b6dddc5e87f2fbca01",
-				"outreachSessions": "4",
-				"notes": "3 outreach sessions and 2 fixed session!",
-				"doc_type": "kpi",
-				"date": "2015-07-06",
-				"originRow": "2",
-				"deliveryRoundID": "BA-1-2015",
-				"driverID": "bashir@example.com",
-				"version": "2.0.0",
-				"worksheetId": "ojuogcm",
-				"spreadsheetId": "1HqYAgB3O9b8jaCUKnvZwAx2C8pr79KwlA2hjemQcU_A",
-				"importedAt": "2015-06-29T11:07:36.040Z",
-				"facility": {
-					"zone": "Test Zone",
-					"lga": "Test LGA",
-					"ward": "Test Ward",
-					"name": "Test HF 1",
-					"id": "TEST HF 101"
-				},
-				"createdOn": "2015-10-16T14:09:59.006Z",
-				"modifiedOn": "2015-10-16T15:33:36.075Z",
-				"antigensKPI": [
-					{
-						"productID": "BCG",
-						"noImmunized": 35
-					},
-					{
-						"productID": "MV",
-						"noImmunized": 0
-					},
-					{
-						"productID": "OPV",
-						"noImmunized": 0
-					},
-					{
-						"productID": "IPV",
-						"noImmunized": 0
-					},
-					{
-						"productID": "TT",
-						"noImmunized": 0
-					},
-					{
-						"productID": "Penta",
-						"noImmunized": 0
-					},
-					{
-						"productID": "HBV",
-						"noImmunized": 0
-					},
-					{
-						"productID": "YF",
-						"noImmunized": 0
-					}
-				]
-			},
-			{
-				"_id": "0f56b4f46d2474b990a43721090007ee",
-				"_rev": "1-f95b34c9f710dc5868320a53575bff96",
-				"outreachSessions": "",
-				"notes": "",
-				"doc_type": "kpi",
-				"date": "2015-07-06",
-				"originRow": "3",
-				"deliveryRoundID": "BA-1-2015",
-				"driverID": "bashir@example.com",
-				"version": "2.0.0",
-				"worksheetId": "ojuogcm",
-				"spreadsheetId": "1HqYAgB3O9b8jaCUKnvZwAx2C8pr79KwlA2hjemQcU_A",
-				"importedAt": "2015-06-29T11:07:36.040Z",
-				"antigensKPI": [
-					{
-						"productID": "BCG",
-						"noImmunized": 0
-					},
-					{
-						"productID": "MV",
-						"noImmunized": 0
-					},
-					{
-						"productID": "OPV",
-						"noImmunized": 0
-					},
-					{
-						"productID": "IPV",
-						"noImmunized": 0
-					},
-					{
-						"productID": "TT",
-						"noImmunized": 0
-					},
-					{
-						"productID": "Penta",
-						"noImmunized": 0
-					},
-					{
-						"productID": "HBV",
-						"noImmunized": 0
-					},
-					{
-						"productID": "YF",
-						"noImmunized": 0
-					}
-				],
-				"facility": {
-					"zone": "Test Zone",
-					"lga": "Test LGA",
-					"ward": "Test Ward",
-					"name": "Test HF 2",
-					"id": "Test HF 102"
-				}
-			}
-		]
-	})
+  .constant('kpiTemplatesMock', [
+    {
+      '_id': 'kpi-template-1',
+      'name': 'Number Immunized',
+      'outreachSessions': '',
+      'notes': '',
+      'doc_type': 'kpi-template',
+      'date': '',
+      'originRow': '',
+      'deliveryRoundID': '',
+      'driverID': '',
+      'version': '2.0.0',
+      'worksheetId': 'ojuogcm',
+      'spreadsheetId': '',
+      'importedAt': '',
+      'antigensKPI': [
+        {
+          'productID': 'BCG',
+          'noImmunized': 0
+        },
+        {
+          'productID': 'MV',
+          'noImmunized': 0
+        },
+        {
+          'productID': 'OPV',
+          'noImmunized': 0
+        },
+        {
+          'productID': 'IPV',
+          'noImmunized': 0
+        },
+        {
+          'productID': 'TT',
+          'noImmunized': 0
+        },
+        {
+          'productID': 'Penta',
+          'noImmunized': 0
+        },
+        {
+          'productID': 'HBV',
+          'noImmunized': 0
+        },
+        {
+          'productID': 'YF',
+          'noImmunized': 0
+        }
+      ],
+      'facility': {
+        'zone': '',
+        'lga': '',
+        'ward': '',
+        'name': '',
+        'id': ''
+      }
+    }
+  ])
+  .constant('kpiInfoMock', {
+    'antigens': [
+      'BCG',
+      'MV',
+      'OPV',
+      'IPV',
+      'TT',
+      'Penta',
+      'HBV',
+      'YF'
+    ],
+    kpiList: [
+      {
+        '_id': '0f56b4f46d2474b990a43721090004cb',
+        '_rev': '30-4d48c732226ac3b6dddc5e87f2fbca01',
+        'outreachSessions': '4',
+        'notes': '3 outreach sessions and 2 fixed session!',
+        'doc_type': 'kpi',
+        'date': '2015-07-06',
+        'originRow': '2',
+        'deliveryRoundID': 'BA-1-2015',
+        'driverID': 'bashir@example.com',
+        'version': '2.0.0',
+        'worksheetId': 'ojuogcm',
+        'spreadsheetId': '1HqYAgB3O9b8jaCUKnvZwAx2C8pr79KwlA2hjemQcU_A',
+        'importedAt': '2015-06-29T11:07:36.040Z',
+        'facility': {
+          'zone': 'Test Zone',
+          'lga': 'Test LGA',
+          'ward': 'Test Ward',
+          'name': 'Test HF 1',
+          'id': 'TEST HF 101'
+        },
+        'createdOn': '2015-10-16T14:09:59.006Z',
+        'modifiedOn': '2015-10-16T15:33:36.075Z',
+        'antigensKPI': [
+          {
+            'productID': 'BCG',
+            'noImmunized': 35
+          },
+          {
+            'productID': 'MV',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'OPV',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'IPV',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'TT',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'Penta',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'HBV',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'YF',
+            'noImmunized': 0
+          }
+        ]
+      },
+      {
+        '_id': '0f56b4f46d2474b990a43721090007ee',
+        '_rev': '1-f95b34c9f710dc5868320a53575bff96',
+        'outreachSessions': '',
+        'notes': '',
+        'doc_type': 'kpi',
+        'date': '2015-07-06',
+        'originRow': '3',
+        'deliveryRoundID': 'BA-1-2015',
+        'driverID': 'bashir@example.com',
+        'version': '2.0.0',
+        'worksheetId': 'ojuogcm',
+        'spreadsheetId': '1HqYAgB3O9b8jaCUKnvZwAx2C8pr79KwlA2hjemQcU_A',
+        'importedAt': '2015-06-29T11:07:36.040Z',
+        'antigensKPI': [
+          {
+            'productID': 'BCG',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'MV',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'OPV',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'IPV',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'TT',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'Penta',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'HBV',
+            'noImmunized': 0
+          },
+          {
+            'productID': 'YF',
+            'noImmunized': 0
+          }
+        ],
+        'facility': {
+          'zone': 'Test Zone',
+          'lga': 'Test LGA',
+          'ward': 'Test Ward',
+          'name': 'Test HF 2',
+          'id': 'Test HF 102'
+        }
+      }
+    ]
+  })
