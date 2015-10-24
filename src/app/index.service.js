2e62fdc789e7d10d228d168e140c157145941c81
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-10-24 10:24:29 +0100

    redirect to login page if 'unauthorized' is emitted and show user warning alert

--- src/app/index.service.js
+++ src/app/index.service.js
@@ -1,7 +1,8 @@
 'use strict'
 
 angular.module('directDeliveryDashboard')
-  .service('indexService', function ($rootScope, $state, editableOptions, log) {
+  .service('indexService', function ($rootScope, $state, editableOptions, log, authService) {
+
     function stateChangeError (event, toState, toParams, fromState, fromParams, err) {
       if (err === 'unauthenticated' || err === 'User not found') {
         return $state.go('login')
@@ -12,10 +13,19 @@ angular.module('directDeliveryDashboard')
       log.error('stateChangeError', event)
     }
 
+    function onDBAuthError () {
+      log.warning('accessDeniedOrExpired')
+      return authService.logout()
+        .then($state.go.bind($state, 'login'))
+    }
+
     this.bootstrap = function () {
       // set xeditable bootstrap theme
       editableOptions.theme = 'bs3'
 
       $rootScope.$on('$stateChangeError', stateChangeError)
+
+      $rootScope.$on('unauthorized', onDBAuthError)
     }
   })
+
