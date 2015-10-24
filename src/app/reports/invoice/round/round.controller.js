21cd2ca888befef30b8908d6c59260d48e15805a
Musa Musa <musakunte@gmail.com>
2015-10-24 11:23:16 +0100

    implemented pagination

--- src/app/reports/invoice/round/round.controller.js
+++ src/app/reports/invoice/round/round.controller.js
@@ -1,16 +1,21 @@
 'use strict'
 
 angular.module('reports')
-  .controller('ReportsRoundCtrl', function ($stateParams, $window, deliveryRounds, dailyDeliveries, drivers, ZONE_CLASS) {
+  .controller('ReportsRoundCtrl', function ($stateParams, $window, drivers, ZONE_CLASS, reportsService) {
+    var vm = this
     var keys = ['driverID', 'date']
     var keyRows = {}
     var lastKeyValues = []
+    vm.pagination = {
+      limit: 10,
+      page: 1,
+      lastPage: 1
+    }
 
-    this.dailyDeliveries = dailyDeliveries
-    this.drivers = drivers
-    this.zoneClass = {}
+    vm.drivers = drivers
+    vm.zoneClass = {}
 
-    this.keyStates = function (delivery, index) {
+    vm.keyStates = function (delivery, index) {
       var states = {}
       var value = ''
       angular.forEach(keys, function (key) {
@@ -31,31 +36,60 @@ angular.module('reports')
       return states
     }
 
-    this.print = function () {
+    vm.print = function () {
       $window.jQuery('#report').print()
     }
 
-    angular.forEach(dailyDeliveries, function (delivery) {
-      var value = ''
-      angular.forEach(keys, function (key) {
-        value += delivery[key]
+    vm.getReport = function (page) {
+      keyRows = {}
+      lastKeyValues = []
+      vm.pagination.page = page || vm.pagination.page
+      vm.pagination.skip = (vm.pagination.page - 1) * vm.pagination.limit
+      if (parseInt(vm.pagination.limit, 10) === 0) {
+        vm.pagination = {}
+      }
+      reportsService.getDailyDeliveries($stateParams.id, vm.pagination)
+        .then(function (response) {
+          vm.dailyDeliveries = response.results
+          vm.pagination.totalItems = response.total
+          vm.pagination.lastPage = isNaN(getLastPage()) ? getLastPage() : 1
+          angular.forEach(vm.dailyDeliveries, function (delivery) {
+            var value = ''
+            angular.forEach(keys, function (key) {
+              value += delivery[key]
+              if (keyRows[value]) {
+                keyRows[value]++
+              } else {
+                keyRows[value] = 1
+              }
+            })
+          })
+        })
+    }
 
-        if (keyRows[value]) {
-          keyRows[value]++
-        } else {
-          keyRows[value] = 1
-        }
-      })
-    })
+    vm.allIn = function () {
+      return (vm.pagination.totalItems <= vm.pagination.limit) || angular.isUndefined(vm.pagination.limit)
+    }
+    vm.hasPrev = function () {
+      return vm.pagination.page > 1
+    }
 
-    if ($stateParams.id) {
-      for (var i = 0; i < deliveryRounds.length; i++) {
-        var round = deliveryRounds[i]
-        if (round.id === $stateParams.id) {
-          this.deliveryRound = round
-          this.zoneClass = ZONE_CLASS[round.state]
-          break
-        }
+    vm.hasNext = function () {
+      return vm.pagination.page !== getLastPage()
+    }
+    vm.selectPage = function (type) {
+      if (type === 'next' && vm.hasNext()) {
+        vm.pagination.page++
+        vm.getReport()
+      } else if (type === 'prev' && vm.hasPrev()) {
+        vm.pagination.page--
+        vm.getReport()
       }
     }
+
+    function getLastPage () {
+      return Math.ceil(vm.pagination.totalItems / vm.pagination.limit)
+    }
+    vm.getReport()
+
   })
