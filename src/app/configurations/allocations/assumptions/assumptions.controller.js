809d1170f19fba637d8628e19fdec47313cd2dc6
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-10-24 12:19:55 +0100

    call template functions correctly, create reusable show table function and update UI

--- src/app/configurations/allocations/assumptions/assumptions.controller.js
+++ src/app/configurations/allocations/assumptions/assumptions.controller.js
@@ -2,8 +2,21 @@
  * Created by ehealthafrica on 7/7/15.
  */
 angular.module('allocations')
-  .controller('AssumptionsCtrl', function (assumptionList, assumptionAddService) {
+  .controller('AssumptionsCtrl', function (assumptionList, assumptionAddService, assumptionService) {
     var vm = this
     vm.assumptionList = assumptionList
-    vm.addAssumption = assumptionAddService.openForm
+    vm.docTypes = assumptionService.DOC_TYPES
+
+    vm.editTemplate = function (template) {
+      assumptionAddService.openForm(template, template.doc_type)
+    }
+
+    vm.createNewTemplate = function (templateType) {
+      return assumptionAddService.openForm(null, templateType)
+    }
+
+    vm.showTable = function () {
+      return vm.assumptionList.length > 0
+    }
+
   })
