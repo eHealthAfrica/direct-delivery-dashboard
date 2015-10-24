e289b8f146e2be52bc8b9830c70f1bc6481ed986
Musa Musa <musakunte@gmail.com>
2015-10-24 11:51:15 +0100

    implemented pagination

--- src/app/reports/invoice/invoice.controller.js
+++ src/app/reports/invoice/invoice.controller.js
@@ -1,7 +1,27 @@
 'use strict'
 
 angular.module('reports')
-  .controller('ReportsAllCtrl', function (deliveryRounds) {
+  .controller('ReportsAllCtrl', function (reportsService) {
     var vm = this
-    vm.deliveryRounds = deliveryRounds
+    vm.pagination = {
+      limit: 10,
+      page: 1
+    }
+
+    vm.getReport = function (page) {
+      vm.pagination.page = page || vm.pagination.page
+      vm.pagination.skip = (vm.pagination.page - 1) * vm.pagination.limit
+      reportsService.getDeliveryRounds(vm.pagination)
+        .then(function (response) {
+          vm.deliveryRounds = response.results
+          vm.pagination.totalItems = response.total
+          vm.pagination.offset = response.offset
+        })
+    }
+
+    vm.getReport()
+
+    vm.allIn = function () {
+      return vm.pagination.totalItems <= vm.pagination.limit
+    }
   })
