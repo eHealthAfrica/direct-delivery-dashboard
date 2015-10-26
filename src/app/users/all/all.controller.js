09e39f2f08dd95d720ddb5cd1b4f17740a6fb70e
Musa Musa <musakunte@gmail.com>
2015-10-26 14:59:59 +0100

    reload users after delete, added error catch block and error alert message

--- src/app/users/all/all.controller.js
+++ src/app/users/all/all.controller.js
@@ -1,17 +1,50 @@
 'use strict'
 
 angular.module('users')
-  .controller('UsersAllCtrl', function (log, usersService, users) {
+  .controller('UsersAllCtrl', function (log, usersService) {
     var vm = this
+
+    vm.statesObject = {}
+    usersService.getStates()
+      .then(function (response) {
+        vm.states = response
+        if (response.length > 0) {
+          var i = response.length
+          while (i--) {
+            vm.statesObject[response[i]._id] = response[i].name
+          }
+        }
+      })
+      .catch(function (reason) {
+        log.error('locationLoadErr')
+      })
+
+    vm.getUsers = function () {
+      usersService.all(vm.selectedState)
+        .then(function (usersObj) {
+          var users = []
+
+          angular.forEach(usersObj, function (user) {
+            users.push(user)
+          })
+
+          vm.allUsers = users
+        })
+        .catch(function (reason) {
+          log.error('userLoadErr')
+        })
+    }
 
     vm.remove = function (user) {
       usersService.remove(user)
         .then(function () {
-          users.splice(users.indexOf(user), 1)
+          vm.getUsers()
           log.success('userRemoved')
         })
         .catch(function (err) {
           log.error('unknownError', err)
         })
     }
+
+    vm.getUsers()
   })
