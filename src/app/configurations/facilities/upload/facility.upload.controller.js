444aa7f9ae3a03dd43259908022fb6cf6325ada9
femi oni <freefony@gmail.com>
2015-10-27 15:33:49 +0100

    all fixed with save method

--- src/app/configurations/facilities/upload/facility.upload.controller.js
+++ src/app/configurations/facilities/upload/facility.upload.controller.js
@@ -40,7 +40,7 @@ angular.module('configurations.facilities')
             for (var r in response) {
               if ((utility.replaceAll(facility.zone, ' ', '-').toUpperCase() === utility.replaceAll(response[r].name, ' ', '-').toUpperCase()) && response[r].level === '3') {
                 facility.ancestors.push(response[r]._id)
-                zone = utility.replaceAll(response[r].name,' ', '_')
+                zone = utility.replaceAll(response[r].name, ' ', '_')
               }
               if ((utility.replaceAll(facility.lganame, ' ', '-') === utility.replaceAll(response[r].name, ' ', '-')) && response[r].level === '4') {
                 facility.ancestors.push(response[r]._id)
@@ -51,23 +51,25 @@ angular.module('configurations.facilities')
                 ward = utility.replaceAll(response[r].name, ' ', '_')
               }
             }
-            facility._id = (_id + '-' + [zone, lga, ward, utility.replaceAll(facility.primary_name, ' ', '_')].join('-')).toUpperCase()
-            console.info(facility)
-            dataToSave.push({
+            var f = {
               ancestors: facility.ancestors,
               osmId: facility.id,
               ownership: facility.ownership,
               latitude: facility.latitude,
               longitude: facility.longitude,
               category: facility.category,
+              name: facility.primary_name,
+              level: '6',
               first_contact_name: '',
               first_contact_phone: '',
               first_contact_email: '',
               second_contact_name: '',
               second_contact_phone: '',
-              second_contact_email: ''
-            })
-            return
+              second_contact_email: '',
+              doc_type: 'location',
+              _id: (_id + '-' + [zone, lga, ward, utility.replaceAll(facility.primary_name, ' ', '_')].join('-')).toUpperCase()
+            }
+            dataToSave.push(f)
           }
         })
     }
@@ -94,8 +96,8 @@ angular.module('configurations.facilities')
     vm.save = function () {
       return locationService.saveMany(dataToSave)
         .then(function (response) {
-          log.success('locationSaveSuccess', response);
-          return response;
+          log.success('locationSaveSuccess', response)
+          return response
         })
         .catch(function (err) {
           log.error('locationSaveErr', err)
