b86b5b9b1f29a44e500005a96eaba7b2c3ce1465
femi oni <freefony@gmail.com>
2015-11-11 11:27:32 +0100

    standard formatting

--- src/app/configurations/locations/configurations.locations.controller.js
+++ src/app/configurations/locations/configurations.locations.controller.js
@@ -11,13 +11,13 @@ angular.module('configurations.locations')
     vm.canSave = false
     var warningFired = false
     vm.finished = function (data) {
-      if(angular.isArray(data)){
-        if(data.length > 0 || (vm.csv.header && data.length ==1)){ //empty files or files with only headers
+      if (angular.isArray(data)) {
+        if (data.length > 0 || (vm.csv.header && data.length === 1)) { // empty files or files with only headers
           vm.canSave = true
           vm.result = data
-          warningFired= false
-        }else{
-          if(!warningFired){
+          warningFired = false
+        } else {
+          if (!warningFired) {
             log.warning('emptyDataUpload', data)
             warningFired = true
           }
@@ -39,7 +39,7 @@ angular.module('configurations.locations')
             'ISO3166-2': results[i]['ISO3166-2'],
             ancestors: [
               (results[i].admin_level_0.length > 2) ? results[i].admin_level_0.length > 2 : null,
-              (results[i].admin_level_1.length > 2) ? results[i].admin_level_1.length > 2 : null,
+              (results[i].admin_level_1.length > 2) ? results[i].admin_level_1.length > 2 : null
             ],
             doc_type: 'location',
             level: results[i].level || '2'
