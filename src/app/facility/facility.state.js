361f1cc6f3de6147c18e99b3534cb7894867a370
Tom Vincent <git@tlvince.com>
2015-11-13 16:13:24 +0000

    fix(facility): default to faulty CCE state
    Connects #304.

--- src/app/facility/facility.state.js
+++ src/app/facility/facility.state.js
@@ -2,20 +2,15 @@
 
 angular.module('facility')
   .config(function ($stateProvider) {
-    $stateProvider.state('facility', {
-      abstract: true,
-      parent: 'index',
-      url: '/facility',
-      controller: function ($state) {
-        $state.go('facility.layout.reports')
-      },
-      data: {
-        label: 'Facility'
-      },
-      templateUrl: 'app/facility/facility.html'
-    })
+    $stateProvider
+      .state('facility', {
+        abstract: true,
+        parent: 'index',
+        url: '/facility',
+        templateUrl: 'app/facility/facility.html'
+      })
       .state('facility.layout', {
-        parent: 'facility',
+        abstract: true,
         views: {
           menu: {
             templateUrl: 'app/facility/menu/menu.html'
