ff5dfb074443ea5f448beaa522cbfb28baccedb5
Tom Vincent <git@tlvince.com>
2015-11-13 16:17:51 +0000

    fix(reports): default to delivery reports state

--- src/app/reports/delivery/delivery-report.states.js
+++ src/app/reports/delivery/delivery-report.states.js
@@ -4,7 +4,10 @@ angular.module('reports')
   .config(function ($stateProvider) {
     $stateProvider.state('reports.layout.delivery', {
       parent: 'reports.layout',
-      url: '/delivery',
+      url: '',
+      data: {
+        label: 'Reports'
+      },
       templateUrl: 'app/reports/delivery/delivery-report.html',
       controller: 'DeliveryReportCtrl',
       controllerAs: 'devRepCtrl'
