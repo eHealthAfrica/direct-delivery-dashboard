63cc2ee163b03d72d0003ec519d4bc21adc902c0
femi oni <freefony@gmail.com>
2016-01-26 15:05:07 +0100

    stop d3 from throwing "Error: Invalid value for <g> attribute transform="translate(NaN,5)"

--- src/app/index.service.js
+++ src/app/index.service.js
@@ -25,9 +25,21 @@ angular.module('directDeliveryDashboard')
         .then($state.go.bind($state, 'login'))
     }
 
+    function stateChangeStart (event, next, current) {
+      if (typeof (current) !== 'undefined') {
+        window.nv.charts = {}
+        window.nv.graphs = []
+        window.nv.logs = {}
+        // remove resize listeners
+        window.onresize = null
+      }
+    }
+
     this.bootstrap = function () {
       // set xeditable bootstrap theme
       editableOptions.theme = 'bs3'
+
+      $rootScope.$on('$stateChangeStart', stateChangeStart)
 
       $rootScope.$on('$stateChangeError', stateChangeError)
 
