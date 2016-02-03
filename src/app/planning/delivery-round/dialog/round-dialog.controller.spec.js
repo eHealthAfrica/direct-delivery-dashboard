897ddec4b7714a55ea8c9914ed9188974f8a4f7a
femi oni <freefony@gmail.com>
2016-02-03 11:03:33 +0100

    saveAndExit and continue functions

--- src/app/planning/delivery-round/dialog/round-dialog.controller.spec.js
+++ src/app/planning/delivery-round/dialog/round-dialog.controller.spec.js
@@ -17,12 +17,12 @@ describe('round-dialog.controller', function () {
   beforeEach(inject(function ($rootScope, $controller, _planningService_) {
     $scope = $rootScope.$new()
     deliveryRound = {
-      state: '',
+      state: 'KANO',
       stateCode: '',
       roundNo: '',
       status: 'PLANNING',
       startDate: new Date(),
-      endDate: ''
+      endDate: new Date()
     }
     stateAdminLevels = []
     planningService = _planningService_
@@ -41,7 +41,9 @@ describe('round-dialog.controller', function () {
         return
       }
     }
-    spyOn(planningService, 'getRoundCode').and.callThrough()
+    spyOn(planningService, ['getRoundCode']).and.callThrough()
+    spyOn(planningService, 'saveRound').and.callThrough()
+    spyOn(planningService, 'createRound').and.callThrough()
     noDeliveryRound = function () {
       deliveryRound = undefined
     }
@@ -91,16 +93,19 @@ describe('round-dialog.controller', function () {
   it('should have a setStateCode function', function () {
     expect(nrdCtrl.setStateCode).toEqual(jasmine.any(Function))
     nrdCtrl.states = [{
-      _id: 'KN-01-2016',
+      _id: 'KN',
       name: 'KANO',
       roundNo: '01'
     }]
     nrdCtrl.setStateCode()
-    expect(nrdCtrl.deliveryRound).toEqual(jasmine.any(Object))
+    expect(nrdCtrl.deliveryRound.stateCode).toEqual(nrdCtrl.states[0]._id)
   })
 
   it('should have a setRoundNumber function', function () {
     expect(nrdCtrl.setRoundNumber).toEqual(jasmine.any(Function))
+    nrdCtrl.deliveryRound._id = 'KN-01-2016'
+    nrdCtrl.setRoundNumber()
+    expect(nrdCtrl.deliveryRound.roundNo).toBe('01')
   })
 
   it('should have a getRoundCode function', function () {
@@ -116,10 +121,28 @@ describe('round-dialog.controller', function () {
 
   it('should have a continue function', function () {
     expect(nrdCtrl.continue).toEqual(jasmine.any(Function))
+    expect(planningService.saveRound).not.toHaveBeenCalled()
+    nrdCtrl.edit = true
+    nrdCtrl.continue()
+    expect(planningService.saveRound).toHaveBeenCalledWith(nrdCtrl.deliveryRound)
+
+    expect(planningService.createRound).not.toHaveBeenCalled()
+    nrdCtrl.edit = false
+    nrdCtrl.continue()
+    expect(planningService.createRound).toHaveBeenCalledWith(nrdCtrl.deliveryRound)
   })
 
   it('should have a saveAndExit function', function () {
     expect(nrdCtrl.saveAndExit).toEqual(jasmine.any(Function))
+    expect(planningService.saveRound).not.toHaveBeenCalled()
+    nrdCtrl.edit = true
+    nrdCtrl.saveAndExit()
+    expect(planningService.saveRound).toHaveBeenCalledWith(nrdCtrl.deliveryRound)
+
+    expect(planningService.createRound).not.toHaveBeenCalled()
+    nrdCtrl.edit = false
+    nrdCtrl.saveAndExit()
+    expect(planningService.createRound).toHaveBeenCalledWith(nrdCtrl.deliveryRound)
   })
 
   it('should have a cancel function', function () {
