9dc26f3e1adff6a8d8737c155f3e1a6274fd9060
Musa Musa <musakunte@gmail.com>
2016-02-04 17:29:23 +0100

    fixed minor error (standeard JS rules)

--- src/app/components/auth/auth.service.mock.js
+++ src/app/components/auth/auth.service.mock.js
@@ -1,11 +1,10 @@
 'use strict';
 
 (function (angular) {
-
   angular.module('authServiceMock', [])
     .service('authService', function ($q) {
       this.getUserSelectedState = function () {
         return $q.when('State1')
       }
     })
-})(angular)
+}(angular))
