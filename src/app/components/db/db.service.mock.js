f2a7ec197280eb4eb19ff6ba9daf10220d7bbc63
Musa Musa <musakunte@gmail.com>
2016-02-04 10:16:19 +0100

    added db service mock

---
+++ src/app/components/db/db.service.mock.js
@@ -0,0 +1,90 @@
+'use strict';
+
+(function (angular) {
+  var deliveryRounds = [
+    {
+      id: 'round1',
+      key: ['State1', '2015-01-01'],
+      value: {
+        roundCode: 'RC1',
+        endDate: new Date('2015-01-13'),
+        id: 'round1',
+        state: 'State1',
+        startDate: new Date('2015-01-01')
+      }
+    }
+  ]
+
+  var byRoundMock = [
+    {
+      'id': 'id1',
+      'key': ['round1', 'ZONEID'],
+      'value': {
+        'status': 'success',
+        'date': '2016-01-29',
+        'zone': 'Zone 1',
+        'lga': 'LGA 1',
+        'ward': 'Kuki',
+        'deliveryRoundID': 'round1',
+        'count': 1,
+        driverID: 'driver.name@domain.com'
+      }
+    },
+    {
+      'id': 'id2',
+      'key': ['round1', 'ZONEID'],
+      'value': {
+        'status': 'success',
+        'date': '2016-01-29',
+        'zone': 'Zone 1',
+        'lga': 'LGA 1',
+        'ward': 'Kuki',
+        'deliveryRoundID': 'round1',
+        'count': 1,
+        driverID: 'driver.name@domain.com'
+      }
+    }
+  ]
+
+  var byDateMock = [
+    {
+      'id': 'id1',
+      'key': ['2015-04-27', 'round1', 'ZONEID'],
+      value: byRoundMock[0].value
+    },
+    {
+      'id': 'id2',
+      'key': ['2015-04-27', 'round1', 'ZONEID'],
+      value: byRoundMock[1].value
+    }
+
+  ]
+
+  angular.module('dbServiceMock', [])
+    .factory('dbService', function ($q) {
+      return {
+        getView: function (view) {
+          var response = null
+
+          if (['reports/delivery-rounds', 'dashboard-delivery-rounds/by-state-and-end-date'].indexOf(view) !== -1) {
+            response = {
+              rows: deliveryRounds
+            }
+          } else if ('dashboard-delivery-rounds/report-by-date') {
+            response = {
+              rows: byDateMock
+            }
+          } else {
+            response = {
+              rows: []
+            }
+          }
+
+          var deferred = $q.defer()
+          deferred.resolve(response)
+
+          return deferred.promise
+        }
+      }
+    })
+}(angular))
